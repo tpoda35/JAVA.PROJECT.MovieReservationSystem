a3b29f4ca5a5034c2dd97f50d0f98998bc9fca31
tpoda <tpoda35@gmail.com>
2025-03-09 19:34:54 +0100

    Initial commit.

---
+++ services/movie/src/main/java/com/moviereservationapi/movie/controller/MovieController.java
@@ -0,0 +1,16 @@
+package com.moviereservationapi.movie.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/movies")
+@RequiredArgsConstructor
+@Slf4j
+public class MovieController {
+
+
+
+}
