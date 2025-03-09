1ff3ce1c41ac94a159ce2f97d96e0f2ce605b678
tpoda <tpoda35@gmail.com>
2025-03-09 20:14:26 +0100

    Started endpoint.

--- services/movie/src/main/java/com/moviereservationapi/movie/controller/MovieController.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/controller/MovieController.java
@@ -1,9 +1,13 @@
 package com.moviereservationapi.movie.controller;
 
+import com.moviereservationapi.movie.dto.MovieDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/movies")
@@ -11,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class MovieController {
 
-
+    @GetMapping
+    public CompletableFuture<MovieDto> getAllMovie(){
+        return null;
+    }
 
 }
