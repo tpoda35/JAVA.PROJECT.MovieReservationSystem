87773d733b9f5632c3d59347e0858b43f4fabc71
tpoda35 <tpoda35@gmail.com>
2025-03-10 23:17:22 +0100

    Added redis config.

---
+++ services/movie/src/main/java/com/moviereservationapi/movie/config/RedisConfiguration.java
@@ -0,0 +1,25 @@
+package com.moviereservationapi.movie.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(10));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
+
+}
