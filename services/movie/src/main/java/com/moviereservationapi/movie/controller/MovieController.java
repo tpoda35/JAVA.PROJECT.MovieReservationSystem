097c438018b40f9e4bd6070e9892af228415bb08
tpoda35 <tpoda35@gmail.com>
2025-03-10 22:33:14 +0100

    Implemented /api/movies endpoint.

--- services/movie/src/main/java/com/moviereservationapi/movie/controller/MovieController.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/controller/MovieController.java
@@ -1,10 +1,14 @@
 package com.moviereservationapi.movie.controller;
 
 import com.moviereservationapi.movie.dto.MovieDto;
+import com.moviereservationapi.movie.service.impl.MovieService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.CompletableFuture;
@@ -15,9 +19,20 @@ import java.util.concurrent.CompletableFuture;
 @Slf4j
 public class MovieController {
 
+    private final MovieService movieService;
+
     @GetMapping
-    public CompletableFuture<MovieDto> getAllMovie(){
-        return null;
+    public CompletableFuture<Page<MovieDto>> getAllMovie(
+            @RequestParam(defaultValue = "0") @NonNull Integer pageNum,
+            @RequestParam(defaultValue = "10") @NonNull Integer pageSize
+    ){
+        log.info("api/movies :: Called endpoint. (pageNum:{}, pageSize:{})", pageNum, pageSize);
+
+        return movieService.getAllMovie(pageNum, pageSize)
+                .thenApply(results -> {
+                    log.info("api/movies :: Found {} movie.", results.getContent().size());
+                    return results;
+                });
     }
 
 }
