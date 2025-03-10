993a015d7f026dba9b18191db87eebf8cf680fc5
tpoda35 <tpoda35@gmail.com>
2025-03-10 22:31:04 +0100

    Added excptionHandling for MovieNotFoundException.

--- services/movie/src/main/java/com/moviereservationapi/movie/exception/GlobalExceptionHandler.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/exception/GlobalExceptionHandler.java
@@ -8,9 +8,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDateTime;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MovieNotFoundException.class)
+    public ResponseEntity<?> handleMovieNotFoundException(
+            MovieNotFoundException ex
+    ) {
+        return ResponseEntity.status(NOT_FOUND).body(
+                new CustomExceptionDto(
+                        ex.getMessage(),
+                        LocalDateTime.now(),
+                        NOT_FOUND.value()
+                )
+        );
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleUncaughtExceptions(
