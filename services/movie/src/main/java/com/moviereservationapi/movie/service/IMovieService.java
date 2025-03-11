ee5b792b99729e0d8277e147ee010834b330aa7a
tpoda35 <tpoda35@gmail.com>
2025-03-11 18:01:21 +0100

    Added addMovie.

--- services/movie/src/main/java/com/moviereservationapi/movie/service/IMovieService.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/service/IMovieService.java
@@ -1,5 +1,6 @@
 package com.moviereservationapi.movie.service;
 
+import com.moviereservationapi.movie.dto.MovieCreateDto;
 import com.moviereservationapi.movie.dto.MovieDto;
 import org.springframework.data.domain.Page;
 
@@ -8,4 +9,5 @@ import java.util.concurrent.CompletableFuture;
 public interface IMovieService {
     CompletableFuture<Page<MovieDto>> getAllMovie(int pageNumber, int pageSize);
     CompletableFuture<MovieDto> getMovie(Long movieId);
+    MovieDto addMovie(MovieCreateDto movieDto);
 }
