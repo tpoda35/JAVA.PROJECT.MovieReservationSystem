059e6b20228f8d246d7b5415aab09f6a7fa74b34
tpoda35 <tpoda35@gmail.com>
2025-03-11 18:02:15 +0100

    Added id property.

--- services/movie/src/main/java/com/moviereservationapi/movie/dto/MovieDto.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/dto/MovieDto.java
@@ -19,6 +19,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class MovieDto implements Serializable {
 
+    private Long id;
+
     @NotNull(message = "Movie title cannot be null.")
     @Length(min = 1, max = 100, message = "Movie name must be between 1 and 100 characters.")
     private String title;
@@ -31,5 +33,4 @@ public class MovieDto implements Serializable {
 
     @Enumerated(EnumType.STRING)
     private MovieGenre movieGenre;
-
 }
