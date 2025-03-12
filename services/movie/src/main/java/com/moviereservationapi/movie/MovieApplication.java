08f7ea49c9923dd3e5cd6954781899833ca55282
tpoda35 <tpoda35@gmail.com>
2025-03-12 17:46:20 +0100

    Enabled async.

--- services/movie/src/main/java/com/moviereservationapi/movie/MovieApplication.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/MovieApplication.java
@@ -3,9 +3,11 @@ package com.moviereservationapi.movie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableCaching
+@EnableAsync
 public class MovieApplication {
 
     public static void main(String[] args) {
