be7fecd890a9ee505706c854baf49e2ebdd91d06
tpoda35 <tpoda35@gmail.com>
2025-03-12 17:45:14 +0100

    Exposed endpoints for getUser and getUsers.

--- services/user/src/main/java/org/moviereservationapi/user/controller/UserController.java
+++ services/user/src/main/java/org/moviereservationapi/user/controller/UserController.java
@@ -1,12 +1,36 @@
 package org.moviereservationapi.user.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.moviereservationapi.user.model.AppUser;
+import org.moviereservationapi.user.service.IUserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
+    private final IUserService userService;
+
+    @GetMapping("/getUser/{userid}")
+    public AppUser getUser(
+            @PathVariable("userid") Long userId
+    ) {
+        log.info("api/users/getUser/userId :: Endpoint called. (userId:{})", userId);
+        return userService.getUser(userId);
+    }
+
+    // Lazy exception
+    @GetMapping("/getUsers")
+    public CompletableFuture<List<AppUser>> getUsers(
+            @RequestParam List<Long> userIds
+            ) {
+        log.info("api/users/getUsers:: Endpoint called. (userId:{})", userIds);
+        return userService.getUsers(userIds);
+    }
 }
