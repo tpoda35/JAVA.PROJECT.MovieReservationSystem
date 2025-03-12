d39a25fad41caf42064d650a8d40242ebc461e9c
tpoda35 <tpoda35@gmail.com>
2025-03-12 17:44:00 +0100

    Extended with more properties.

--- services/user/src/main/java/org/moviereservationapi/user/model/AppUser.java
+++ services/user/src/main/java/org/moviereservationapi/user/model/AppUser.java
@@ -1,15 +1,11 @@
 package org.moviereservationapi.user.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -26,4 +22,31 @@ public class AppUser {
     @Email(message = "Invalid email format.")
     @NotBlank(message = "Email field cannot be empty.")
     private String email;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "user_movies",
+            joinColumns = @JoinColumn(name = "user_id")
+    )
+    @Column(name = "movie_id")
+    @ToString.Exclude
+    private List<Long> movieIds;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "user_reviews",
+            joinColumns = @JoinColumn(name = "user_id")
+    )
+    @Column(name = "review_id")
+    @ToString.Exclude
+    private List<Long> reviewIds;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "user_payments",
+            joinColumns = @JoinColumn(name = "user_id")
+    )
+    @Column(name = "payment_id")
+    @ToString.Exclude
+    private List<Long> paymentIds;
 }
