5e2494f3d08a7d055e4260463eb757f380ab599d
tpoda35 <tpoda35@gmail.com>
2025-03-12 17:47:08 +0100

    Added getUser and getUsers.

--- services/user/src/main/java/org/moviereservationapi/user/service/IUserService.java
+++ services/user/src/main/java/org/moviereservationapi/user/service/IUserService.java
@@ -1,4 +1,11 @@
 package org.moviereservationapi.user.service;
 
+import org.moviereservationapi.user.model.AppUser;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 public interface IUserService {
+    AppUser getUser(Long userId);
+    CompletableFuture<List<AppUser>> getUsers(List<Long> userIds);
 }
