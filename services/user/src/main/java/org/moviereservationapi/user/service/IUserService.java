307eb1babe639bcec56eecbbc3e0b33e44e63b1c
tpoda <tpoda35@gmail.com>
2025-03-13 19:07:28 +0100

    Removed getusers method.

--- services/user/src/main/java/org/moviereservationapi/user/service/IUserService.java
+++ services/user/src/main/java/org/moviereservationapi/user/service/IUserService.java
@@ -2,10 +2,8 @@ package org.moviereservationapi.user.service;
 
 import org.moviereservationapi.user.model.AppUser;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public interface IUserService {
-    AppUser getUser(Long userId);
-    CompletableFuture<List<AppUser>> getUsers(List<Long> userIds);
+    CompletableFuture<AppUser> getUser(Long userId);
 }
