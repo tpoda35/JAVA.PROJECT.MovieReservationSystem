a15546207766bcdca5d34ba8c5b2debdd615ed90
tpoda <tpoda35@gmail.com>
2025-03-13 19:09:18 +0100

    Designed the dto.

--- services/movie/src/main/java/com/moviereservationapi/movie/dto/ReviewDto.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/dto/ReviewDto.java
@@ -1,4 +1,20 @@
 package com.moviereservationapi.movie.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReviewDto {
+
+    private String email;
+    private String content;
+    private LocalDateTime createdAt;
+
 }
