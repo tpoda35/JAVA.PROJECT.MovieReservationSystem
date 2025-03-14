07732fd8c40773f467a1751f61de60c66b750667
tpoda <tpoda35@gmail.com>
2025-03-14 20:16:38 +0100

    Modified the endpoint.

--- services/user/src/main/java/org/moviereservationapi/user/controller/UserController.java
+++ services/user/src/main/java/org/moviereservationapi/user/controller/UserController.java
@@ -2,9 +2,12 @@ package org.moviereservationapi.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.moviereservationapi.user.model.AppUser;
+import org.moviereservationapi.user.dto.AppUserDto;
 import org.moviereservationapi.user.service.IUserService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -17,11 +20,11 @@ public class UserController {
 
     private final IUserService userService;
 
-    @GetMapping("/getUser/{userid}")
-    public CompletableFuture<AppUser> getUser(
-            @PathVariable("userid") Long userId
+    @GetMapping("/getUsers")
+    public CompletableFuture<List<AppUserDto>> getUsers(
+            @RequestParam List<Long> userIds
     ) {
-        log.info("api/users/getUser/userId :: Endpoint called. (userId:{})", userId);
-        return userService.getUser(userId);
+        log.info("api/users/getUser/userId :: Endpoint called. (userIds:{})", userIds);
+        return userService.getUsers(userIds);
     }
 }
