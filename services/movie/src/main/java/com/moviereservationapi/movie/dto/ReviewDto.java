c05c6ecb80fb07481534862b8d57d76d31fcaee7
tpoda <tpoda35@gmail.com>
2025-03-14 20:16:01 +0100

    Added serializable.

--- services/movie/src/main/java/com/moviereservationapi/movie/dto/ReviewDto.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/dto/ReviewDto.java
@@ -5,13 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ReviewDto {
+public class ReviewDto implements Serializable {
 
     private String email;
     private String content;
