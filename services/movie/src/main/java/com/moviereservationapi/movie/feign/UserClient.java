6a40b68d0bdaf6c036d6955f44a690b79edaaa90
tpoda <tpoda35@gmail.com>
2025-03-14 20:16:29 +0100

    Added feign client endpoint (not working yet)

--- services/movie/src/main/java/com/moviereservationapi/movie/feign/UserClient.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/feign/UserClient.java
@@ -1,16 +1,18 @@
 package com.moviereservationapi.movie.feign;
 
-import com.moviereservationapi.movie.feignResponse.AppUserResponse;
+import com.moviereservationapi.movie.feignResponse.AppUserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @FeignClient("USER-SERVICE")
 public interface UserClient {
 
-    @GetMapping("/getUser/{userid}")
-    CompletableFuture<AppUserResponse> getUser(@PathVariable("userid") Long userId);
+    // problem
+    @GetMapping("/getUsers")
+    CompletableFuture<List<AppUserDto>> getUsers(@RequestParam List<Long> userIds);
 
 }
