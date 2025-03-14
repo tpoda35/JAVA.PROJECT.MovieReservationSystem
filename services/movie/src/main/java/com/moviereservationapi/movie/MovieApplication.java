7ded5b13ce4cf74666895f056af0be8b0d3bf99f
tpoda <tpoda35@gmail.com>
2025-03-14 20:14:01 +0100

    Added Feign Client support.

--- services/movie/src/main/java/com/moviereservationapi/movie/MovieApplication.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/MovieApplication.java
@@ -3,11 +3,13 @@ package com.moviereservationapi.movie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableCaching
 @EnableAsync
+@EnableFeignClients
 public class MovieApplication {
 
     public static void main(String[] args) {
