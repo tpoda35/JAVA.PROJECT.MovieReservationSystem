a4c4a799a0dfb2bcb7f276423f049205cb4f21a4
tpoda <tpoda35@gmail.com>
2025-03-16 14:16:39 +0100

    Added logging.

--- services/movie/src/main/java/com/moviereservationapi/movie/service/impl/CinemaService.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/service/impl/CinemaService.java
@@ -26,35 +26,44 @@ public class CinemaService implements ICinemaService {
 
     private final CinemaRepository cinemaRepository;
     private final CacheManager cacheManager;
+
     private final ConcurrentHashMap<String, Object> locks = new ConcurrentHashMap<>();
 
     @Override
     @Async
     public CompletableFuture<Page<CinemaDto>> getAllCinema(int pageNum, int pageSize) {
-        String cacheKey = String.format("movies_page_%d_size_%d", pageNum, pageSize);
+        String cacheKey = String.format("cinemas_page_%d_size_%d", pageNum, pageSize);
         Cache cache = cacheManager.getCache("cinemas");
 
         ValueWrapper cachedResult;
+        log.info("api/cinemas :: Checking cache (1) for key '{}'.", cacheKey);
         if (cache != null && (cachedResult = cache.get(cacheKey)) != null){
+            log.info("api/cinemas :: Cache HIT for key '{}'. Returning cache.", cacheKey);
             return CompletableFuture.completedFuture(
                     (Page<CinemaDto>) cachedResult.get()
             );
         }
+        log.info("api/cinemas :: Cache MISS for key '{}'.", cacheKey);
 
         Object lock = locks.computeIfAbsent(cacheKey, k -> new Object());
         synchronized (lock) {
             try {
+                log.info("api/cinemas :: Checking cache (2) for key '{}'.", cacheKey);
                 if (cache != null && (cachedResult = cache.get(cacheKey)) != null){
+                    log.info("api/cinemas :: Cache HIT for key '{}'. Returning cache.", cacheKey);
                     return CompletableFuture.completedFuture(
                             (Page<CinemaDto>) cachedResult.get()
                     );
                 }
+                log.info("api/cinemas :: Cache MISS for key '{}'. Fetching from DB...", cacheKey);
 
                 Page<Cinema> cinemas = cinemaRepository.findAll(PageRequest.of(pageNum, pageSize));
                 if (cinemas.isEmpty()) {
+                    log.info("api/cinemas :: No cinemas found.");
                     throw new CinemaNotFoundException("There's no cinema found.");
                 }
 
+                log.info("api/cinemas :: Found {} cinemas. Caching data for key '{}'.", cinemas.getTotalElements(), cacheKey);
                 Page<CinemaDto> results = cinemas.map(CinemaMapper::fromCinemaToDto);
                 if (cache != null) {
                     cache.put(cacheKey, results);
@@ -70,28 +79,38 @@ public class CinemaService implements ICinemaService {
     @Override
     @Async
     public CompletableFuture<CinemaDto> getCinema(Long cinemaId) {
-        String cacheKey = String.format("movie_%d", cinemaId);
+        String cacheKey = String.format("cinema_%d", cinemaId);
         Cache cache = cacheManager.getCache("cinema");
 
         ValueWrapper cachedResult;
+        log.info("api/cinemas/cinemaId :: Checking cache (1) for key '{}'.", cacheKey);
         if (cache != null && (cachedResult = cache.get(cacheKey)) != null){
+            log.info("api/cinemas/cinemaId :: Cache HIT for key '{}'. Returning cache.", cacheKey);
             return CompletableFuture.completedFuture(
                     (CinemaDto) cachedResult.get()
             );
         }
+        log.info("api/cinemas/cinemaId :: Cache MISS for key '{}'.", cacheKey);
 
         Object lock = locks.computeIfAbsent(cacheKey, k -> new Object());
         synchronized (lock) {
             try {
+                log.info("api/cinemas/cinemaId :: Checking cache (2) for key '{}'.", cacheKey);
                 if (cache != null && (cachedResult = cache.get(cacheKey)) != null){
+                    log.info("api/cinemas/cinemaId :: Cache HIT for key '{}'. Returning cache.", cacheKey);
                     return CompletableFuture.completedFuture(
                             (CinemaDto) cachedResult.get()
                     );
                 }
+                log.info("api/cinemas/cinemaId :: Cache MISS for key '{}'. Fetching from DB...", cacheKey);
 
                 Cinema cinema = cinemaRepository.findById(cinemaId)
-                        .orElseThrow(() -> new CinemaNotFoundException("There's no cinema found."));
+                        .orElseThrow(() -> {
+                            log.info("api/cinemas/cinemaId :: Cinema not found with the id of {}.", cinemaId);
+                            return new CinemaNotFoundException("There's no cinema found.");
+                        });
 
+                log.info("api/cinemas/cinemaId :: Cinema found with the id of {}. Caching data for key '{}'", cinemaId, cacheKey);
                 CinemaDto result = CinemaMapper.fromCinemaToDto(cinema);
                 if (cache != null) {
                     cache.put(cacheKey, result);
