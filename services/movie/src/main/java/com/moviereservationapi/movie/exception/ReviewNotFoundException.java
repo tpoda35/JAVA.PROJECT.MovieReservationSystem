7d1e9ab4b367e238f031fdd438a23aea83b04baf
tpoda <tpoda35@gmail.com>
2025-03-16 13:23:57 +0100

    Removed.

--- services/movie/src/main/java/com/moviereservationapi/movie/exception/ReviewNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.moviereservationapi.movie.exception;
-
-public class ReviewNotFoundException extends RuntimeException {
-    public ReviewNotFoundException(String message) {
-        super(message);
-    }
-}
