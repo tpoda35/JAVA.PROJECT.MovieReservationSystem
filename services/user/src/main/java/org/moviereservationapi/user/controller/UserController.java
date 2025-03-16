bb12813d054568e58381ed748e51cb527840fe88
tpoda <tpoda35@gmail.com>
2025-03-16 11:48:21 +0100

    Removed endpoint.

--- services/user/src/main/java/org/moviereservationapi/user/controller/UserController.java
+++ services/user/src/main/java/org/moviereservationapi/user/controller/UserController.java
@@ -2,15 +2,9 @@ package org.moviereservationapi.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.moviereservationapi.user.dto.AppUserDto;
 import org.moviereservationapi.user.service.IUserService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,12 +13,4 @@ import java.util.concurrent.CompletableFuture;
 public class UserController {
 
     private final IUserService userService;
-
-    @GetMapping("/getUsers")
-    public List<AppUserDto> getUsers(
-            @RequestParam List<Long> userIds
-    ) {
-        log.info("api/users/getUser/userId :: Endpoint called. (userIds:{})", userIds);
-        return userService.getUsers(userIds);
-    }
 }
