e4e8b1dd6ee07c24b252548883ac49d4d91728ff
tpoda <tpoda35@gmail.com>
2025-03-16 13:24:18 +0100

    Modified a variable name.

--- services/movie/src/main/java/com/moviereservationapi/movie/service/IMovieService.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/service/IMovieService.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Page;
 import java.util.concurrent.CompletableFuture;
 
 public interface IMovieService {
-    CompletableFuture<Page<MovieDto>> getAllMovie(int pageNumber, int pageSize);
+    CompletableFuture<Page<MovieDto>> getAllMovie(int pageNum, int pageSize);
     CompletableFuture<MovieDto> getMovie(Long movieId);
     // Role required.
     MovieDto addMovie(MovieManageDto movieManageDto);
