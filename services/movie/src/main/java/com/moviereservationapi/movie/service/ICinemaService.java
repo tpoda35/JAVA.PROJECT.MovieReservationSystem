a8eaf2043a9e883f65c36f55fbf8669964b8a7e6
tpoda <tpoda35@gmail.com>
2025-03-16 14:51:43 +0100

    added addCinema.

--- services/movie/src/main/java/com/moviereservationapi/movie/service/ICinemaService.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/service/ICinemaService.java
@@ -1,6 +1,7 @@
 package com.moviereservationapi.movie.service;
 
 import com.moviereservationapi.movie.dto.CinemaDto;
+import com.moviereservationapi.movie.dto.CinemaManageDto;
 import org.springframework.data.domain.Page;
 
 import java.util.concurrent.CompletableFuture;
@@ -8,4 +9,5 @@ import java.util.concurrent.CompletableFuture;
 public interface ICinemaService {
     CompletableFuture<Page<CinemaDto>> getAllCinema(int pageNum, int pageSize);
     CompletableFuture<CinemaDto> getCinema(Long cinemaId);
+    CinemaDto addCinema(Long cinemaId, CinemaManageDto cinemaManageDto);
 }
