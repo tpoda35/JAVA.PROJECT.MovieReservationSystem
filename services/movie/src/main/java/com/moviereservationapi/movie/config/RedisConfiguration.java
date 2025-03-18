2c5d23c10452c0a57ae37008dc153faf3d2e5fa9
tpoda35 <tpoda35@gmail.com>
2025-03-18 20:25:31 +0100

    Removed cinema caches.

--- services/movie/src/main/java/com/moviereservationapi/movie/config/RedisConfiguration.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/config/RedisConfiguration.java
@@ -23,8 +23,6 @@ public class RedisConfiguration {
         Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
         cacheConfigurations.put("movies", cacheConfiguration.entryTtl(Duration.ofDays(1)));
         cacheConfigurations.put("movie", cacheConfiguration.entryTtl(Duration.ofDays(1)));
-        cacheConfigurations.put("cinemas", cacheConfiguration.entryTtl(Duration.ofDays(1)));
-        cacheConfigurations.put("cinema", cacheConfiguration.entryTtl(Duration.ofDays(1)));
 
         return RedisCacheManager.builder(redisConnectionFactory)
                 .cacheDefaults(cacheConfiguration)
