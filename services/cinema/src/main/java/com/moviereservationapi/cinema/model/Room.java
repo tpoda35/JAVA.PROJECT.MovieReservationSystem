e89b5c571b15068e82339c13a226c5f9f71ce337
tpoda35 <tpoda35@gmail.com>
2025-03-18 20:27:13 +0100

    Added ManyToOne and an ElementCollection.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/model/Room.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/model/Room.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -24,6 +25,18 @@ public class Room {
 
     private Integer totalSeat;
 
+    @ManyToOne
+    @JoinColumn(name = "cinema_id")
+    private Cinema cinema;
+
     @OneToMany(mappedBy = "room", cascade = CascadeType.ALL, orphanRemoval = true)
     private Collection<Seat> seat;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "room_showtime",
+            joinColumns = @JoinColumn(name = "room_id")
+    )
+    @Column(name = "showtime_id")
+    private List<Long> showtimeIds;
 }
