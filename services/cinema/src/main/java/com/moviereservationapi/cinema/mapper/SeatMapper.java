93e7343163c14a4c23a972a34c543fa2be968ba5
tpoda35 <tpoda35@gmail.com>
2025-03-19 22:38:49 +0100

    Added mapping.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/mapper/SeatMapper.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/mapper/SeatMapper.java
@@ -1,15 +1,26 @@
 package com.moviereservationapi.cinema.mapper;
 
 import com.moviereservationapi.cinema.dto.SeatDto;
+import com.moviereservationapi.cinema.dto.SeatManageDto;
+import com.moviereservationapi.cinema.model.Room;
 import com.moviereservationapi.cinema.model.Seat;
 
 public class SeatMapper {
 
     public static SeatDto fromSeatToDto(Seat seat) {
         return SeatDto.builder()
+                .id(seat.getId())
                 .seatRow(seat.getSeatRow())
                 .seatNumber(seat.getSeatNumber())
                 .roomName(seat.getRoom().getName())
+                .build();
+    }
+
+    public static Seat fromManageDtoToSeat(SeatManageDto seatManageDto, Room room) {
+        return Seat.builder()
+                .seatRow(seatManageDto.getSeatRow())
+                .seatNumber(seatManageDto.getSeatNumber())
+                .room(room)
                 .build();
     }
 
