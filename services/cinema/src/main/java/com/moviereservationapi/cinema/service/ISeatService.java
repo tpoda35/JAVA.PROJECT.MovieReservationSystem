6477de4e4c83aaf95d54412ea2dee367b50a55ae
tpoda35 <tpoda35@gmail.com>
2025-03-19 18:59:21 +0100

    Added getAllSeat.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/service/ISeatService.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/service/ISeatService.java
@@ -1,10 +1,11 @@
 package com.moviereservationapi.cinema.service;
 
 import com.moviereservationapi.cinema.dto.SeatDto;
-import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public interface ISeatService {
-    public CompletableFuture<SeatDto> getSeat(Long seatId);
+    CompletableFuture<SeatDto> getSeat(Long seatId);
+    CompletableFuture<List<SeatDto>> getAllSeat(Long roomId);
 }
