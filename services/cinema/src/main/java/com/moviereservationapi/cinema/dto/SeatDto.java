4a150ec0469b4b6ec4fc59d1e6a38fb385cb8221
tpoda35 <tpoda35@gmail.com>
2025-03-19 21:22:54 +0100

    Added serializable.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/dto/SeatDto.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/dto/SeatDto.java
@@ -1,19 +1,27 @@
 package com.moviereservationapi.cinema.dto;
 
-import com.moviereservationapi.cinema.model.Room;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class SeatDto {
+public class SeatDto implements Serializable {
 
+    @NotBlank(message = "Seat row field cannot be empty.")
     private String seatRow;
+
+    @NotNull(message = "Seat number field cannot be empty.")
     private Integer seatNumber;
-    private Room room;
+
+    @NotNull(message = "Room field cannot be empty.")
+    private String roomName;
 
 }
