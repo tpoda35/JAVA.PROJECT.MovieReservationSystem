329df8260fff58bc62fb02d0a500734fd6aebc7e
tpoda35 <tpoda35@gmail.com>
2025-03-19 19:00:19 +0100

    Added new query.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/repository/SeatRepository.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/repository/SeatRepository.java
@@ -4,6 +4,11 @@ import com.moviereservationapi.cinema.model.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+    List<Seat> findSeatsByRoomId(Long roomId);
+
 }
