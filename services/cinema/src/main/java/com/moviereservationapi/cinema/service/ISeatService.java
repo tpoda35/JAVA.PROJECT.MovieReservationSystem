c66559a66989e111832ec4183bea3a7e8d64f244
tpoda35 <tpoda35@gmail.com>
2025-03-19 22:37:30 +0100

    Renamed and added methods.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/service/ISeatService.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/service/ISeatService.java
@@ -1,11 +1,13 @@
 package com.moviereservationapi.cinema.service;
 
 import com.moviereservationapi.cinema.dto.SeatDto;
+import com.moviereservationapi.cinema.dto.SeatManageDto;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public interface ISeatService {
     CompletableFuture<SeatDto> getSeat(Long seatId);
-    CompletableFuture<List<SeatDto>> getAllSeat(Long roomId);
+    CompletableFuture<List<SeatDto>> getAllSeatByRoom(Long roomId);
+    SeatDto addSeat(SeatManageDto seatManageDto);
 }
