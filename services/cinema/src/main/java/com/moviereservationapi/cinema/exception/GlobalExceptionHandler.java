ff36bd4f70709ad3a56e9b07121b28f502aaf268
tpoda35 <tpoda35@gmail.com>
2025-03-19 22:37:14 +0100

    Modified return status code.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/exception/GlobalExceptionHandler.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.moviereservationapi.cinema.exception;
 
 import com.moviereservationapi.cinema.dto.CustomExceptionDto;
-import com.moviereservationapi.cinema.dto.SeatNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDateTime;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,11 +17,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<CustomExceptionDto> handleSeatNotFoundException(
             SeatNotFoundException ex
     ) {
-        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(
+        return ResponseEntity.status(NOT_FOUND).body(
                 new CustomExceptionDto(
                         ex.getMessage(),
                         LocalDateTime.now(),
-                        INTERNAL_SERVER_ERROR.value()
+                        NOT_FOUND.value()
                 )
         );
     }
