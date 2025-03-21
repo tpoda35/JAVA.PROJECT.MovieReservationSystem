cdc0e57c79c875cbfbe3171fb3e072c364f5f2c0
tpoda <tpoda35@gmail.com>
2025-03-21 14:19:02 +0100

    Added properties.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/model/Showtime.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/model/Showtime.java
@@ -1,10 +1,7 @@
 package com.moviereservationapi.showtime.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +9,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 @Data
@@ -32,5 +30,17 @@ public class Showtime {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime endTime;
+
+    private Long movieId;
+
+    private Long roomId;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "showtime_reservations",
+            joinColumns = @JoinColumn(name = "showtime_id")
+    )
+    @Column(name = "reservation_id")
+    private List<Long> reservationIds;
 
 }
