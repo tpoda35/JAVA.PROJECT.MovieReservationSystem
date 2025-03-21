7d468fbd6b8d353978fdf76f0cd0b6d5eb20a2fa
tpoda <tpoda35@gmail.com>
2025-03-21 19:13:15 +0100

    Added getShowtime and addShowtime.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/service/IShowtimeService.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/service/IShowtimeService.java
@@ -1,10 +1,13 @@
 package com.moviereservationapi.showtime.service;
 
 import com.moviereservationapi.showtime.dto.ShowtimeDto;
+import com.moviereservationapi.showtime.dto.ShowtimeManageDto;
 import org.springframework.data.domain.Page;
 
 import java.util.concurrent.CompletableFuture;
 
 public interface IShowtimeService {
     CompletableFuture<Page<ShowtimeDto>> getShowtimes(int pageNum, int pageSize);
+    CompletableFuture<ShowtimeDto> getShowtime(Long showtimeId);
+    ShowtimeDto addShowtime(ShowtimeManageDto showtimeManageDto);
 }
