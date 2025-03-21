08e1859ac9f690ddb2019fc0ea7db0863f79f7ec
tpoda <tpoda35@gmail.com>
2025-03-21 14:19:59 +0100

    Modified RestController to RestControllerAdvice.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/exception/GlobalExceptionHandler.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/exception/GlobalExceptionHandler.java
@@ -3,14 +3,14 @@ package com.moviereservationapi.showtime.exception;
 import com.moviereservationapi.showtime.dto.CustomExceptionDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ShowtimeNotFoundException.class)
