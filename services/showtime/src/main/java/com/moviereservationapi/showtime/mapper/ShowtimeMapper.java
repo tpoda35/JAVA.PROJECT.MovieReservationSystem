7867766adce5dc9447cbb00826640aaeb2d9b557
tpoda <tpoda35@gmail.com>
2025-03-22 08:58:22 +0100

    Added custom mapper.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/mapper/ShowtimeMapper.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/mapper/ShowtimeMapper.java
@@ -1,6 +1,7 @@
 package com.moviereservationapi.showtime.mapper;
 
 import com.moviereservationapi.showtime.dto.ShowtimeDto;
+import com.moviereservationapi.showtime.dto.ShowtimeManageDto;
 import com.moviereservationapi.showtime.model.Showtime;
 
 public class ShowtimeMapper {
@@ -10,6 +11,15 @@ public class ShowtimeMapper {
                 .id(showtime.getId())
                 .startTime(showtime.getStartTime())
                 .endTime(showtime.getEndTime())
+                .build();
+    }
+
+    public static Showtime fromManageDtoToShowtime(ShowtimeManageDto showtimeManageDto) {
+        return Showtime.builder()
+                .startTime(showtimeManageDto.getStartTime())
+                .endTime(showtimeManageDto.getEndTime())
+                .movieId(showtimeManageDto.getMovieId())
+                .roomId(showtimeManageDto.getRoomId())
                 .build();
     }
 
