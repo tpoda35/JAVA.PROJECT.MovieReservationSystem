8fbe362d20e22f2e489a9c34ec666cd998a009ef
tpoda <tpoda35@gmail.com>
2025-03-22 08:57:52 +0100

    added feign.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/ShowtimeApplication.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/ShowtimeApplication.java
@@ -3,11 +3,13 @@ package com.moviereservationapi.showtime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
 @EnableCaching
+@EnableFeignClients
 public class ShowtimeApplication {
 
 	public static void main(String[] args) {
