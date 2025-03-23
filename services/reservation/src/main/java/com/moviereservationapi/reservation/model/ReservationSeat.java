cd77631b55c476d16da6c438b17734e0b3d41fb6
tpoda <tpoda35@gmail.com>
2025-03-23 12:30:17 +0100

    Added unique constraint.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/model/ReservationSeat.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/model/ReservationSeat.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"reservation_id", "seatId"}))
 public class ReservationSeat {
 
     @Id
