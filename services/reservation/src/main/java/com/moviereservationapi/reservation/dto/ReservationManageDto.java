6b62e12d3d638d584d785b4a983df348d318a378
tpoda <tpoda35@gmail.com>
2025-03-23 12:30:05 +0100

    Added properties.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/dto/ReservationManageDto.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/dto/ReservationManageDto.java
@@ -5,12 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReservationManageDto {
 
-
+    private Long showtimeId;
+    private List<Long> seatIds;
+    private Long userId;
 
 }
