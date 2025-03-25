ddeeec8ae001be0ab7c56a92469fb067885e1013
tpoda35 <tpoda35@gmail.com>
2025-03-25 20:00:11 +0100

    Added feign call.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/feign/ShowtimeClient.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/feign/ShowtimeClient.java
@@ -1,7 +1,13 @@
 package com.moviereservationapi.reservation.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "showtime-service", url = "http://localhost:8092")
 public interface ShowtimeClient {
+
+    @GetMapping("/api/showtimes/feign/showtimeExists/{showtimeId}")
+    Boolean showtimeExists(@PathVariable("showtimeId") Long showtimeId);
+
 }
