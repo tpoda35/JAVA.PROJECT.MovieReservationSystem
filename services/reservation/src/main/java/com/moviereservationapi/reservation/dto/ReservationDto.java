13e72bdb9a88058f13ff7a6e6b74572b8fa0b480
tpoda35 <tpoda35@gmail.com>
2025-03-25 19:58:14 +0100

    Filled the dto with properties.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/dto/ReservationDto.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/dto/ReservationDto.java
@@ -1,4 +1,28 @@
 package com.moviereservationapi.reservation.dto;
 
+import com.moviereservationapi.reservation.Enum.PaymentStatus;
+import com.moviereservationapi.reservation.model.ReservationSeat;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReservationDto {
+
+    private LocalDateTime reservationTime;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    private List<ReservationSeat> reservationSeats;
+
 }
