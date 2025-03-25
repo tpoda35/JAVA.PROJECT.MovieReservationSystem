a2d1844c1771ee10c36905931477902a631ecdd1
tpoda35 <tpoda35@gmail.com>
2025-03-25 20:00:03 +0100

    Added query for null check.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/repository/SeatRepository.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/repository/SeatRepository.java
@@ -2,8 +2,15 @@ package com.moviereservationapi.cinema.repository;
 
 import com.moviereservationapi.cinema.model.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+    @Query("SELECT COUNT(e) FROM Seat e WHERE e.id IN :ids")
+    long countByIdIn(List<Long> ids);
+
 }
