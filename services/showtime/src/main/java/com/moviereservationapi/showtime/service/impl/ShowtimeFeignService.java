8022eb919289d18958e54185910e8d30eaa30323
tpoda35 <tpoda35@gmail.com>
2025-03-25 20:00:31 +0100

    Added feign showtimeExists.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/service/impl/ShowtimeFeignService.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/service/impl/ShowtimeFeignService.java
@@ -1,5 +1,7 @@
 package com.moviereservationapi.showtime.service.impl;
 
+import com.moviereservationapi.showtime.repository.ShowtimeRepository;
+import com.moviereservationapi.showtime.service.IShowtimeFeignService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -7,5 +9,14 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class ShowtimeFeignService {
+public class ShowtimeFeignService implements IShowtimeFeignService {
+
+    private final ShowtimeRepository showtimeRepository;
+
+    @Override
+    public Boolean showtimeExists(Long showtimeId) {
+        boolean exists = showtimeRepository.existsById(showtimeId);
+        log.info("(Feign call) Showtime with the id of {} exists: {}.", showtimeId, exists);
+        return exists;
+    }
 }
