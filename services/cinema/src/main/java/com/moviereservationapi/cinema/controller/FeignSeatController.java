fd4af204e954c474ca3486ae001d822685ec49ea
tpoda35 <tpoda35@gmail.com>
2025-03-25 19:56:59 +0100

    Added feign call.

---
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/controller/FeignSeatController.java
@@ -0,0 +1,28 @@
+package com.moviereservationapi.cinema.controller;
+
+import com.moviereservationapi.cinema.service.ISeatFeignService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/seats/feign")
+@RequiredArgsConstructor
+@Slf4j
+public class FeignSeatController {
+
+    private final ISeatFeignService seatFeignService;
+
+    @GetMapping("/seatsExists")
+    public Boolean seatsExists(
+            @RequestParam List<Long> seatIds
+    ) {
+        return seatFeignService.seatsExists(seatIds);
+    }
+
+}
