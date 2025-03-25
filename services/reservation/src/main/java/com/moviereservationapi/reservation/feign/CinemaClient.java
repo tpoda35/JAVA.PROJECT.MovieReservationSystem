732aa88d72a74daf3cc1ee296f20e25bb8084276
tpoda35 <tpoda35@gmail.com>
2025-03-25 19:56:51 +0100

    Added feign call.

---
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/feign/CinemaClient.java
@@ -0,0 +1,15 @@
+package com.moviereservationapi.reservation.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "cinema-service", url = "http://localhost:8091")
+public interface CinemaClient {
+
+    @GetMapping("/api/seats/feign/seatsExists")
+    Boolean seatsExists(@RequestParam List<Long> seatIds);
+
+}
