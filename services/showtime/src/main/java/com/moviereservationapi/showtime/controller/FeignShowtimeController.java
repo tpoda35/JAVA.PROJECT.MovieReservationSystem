e0d749b40457c4e516405569be626882adbf5c95
tpoda <tpoda35@gmail.com>
2025-03-28 12:39:00 +0100

    Modified endpoint name and added addShowtimeReservation endpoint.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/controller/FeignShowtimeController.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/controller/FeignShowtimeController.java
@@ -1,12 +1,10 @@
 package com.moviereservationapi.showtime.controller;
 
+import com.moviereservationapi.showtime.dto.ShowtimeDto;
 import com.moviereservationapi.showtime.service.IShowtimeFeignService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/showtimes/feign")
@@ -16,11 +14,19 @@ public class FeignShowtimeController {
 
     private final IShowtimeFeignService showtimeFeignService;
 
-    @GetMapping("/showtimeExists/{showtimeId}")
-    public Boolean showtimeExists(
+    @GetMapping("/getShowtime/{showtimeId}")
+    public ShowtimeDto getShowtime(
             @PathVariable("showtimeId") Long showtimeId
     ) {
-        return showtimeFeignService.showtimeExists(showtimeId);
+        return showtimeFeignService.getShowtime(showtimeId);
+    }
+
+    @PostMapping("/addShowtimeReservation/{showtimeId}/{reservationId}")
+    public void addShowtimeReservation(
+            @PathVariable("showtimeId") Long showtimeId,
+            @PathVariable("reservationId") Long reservationId
+    ) {
+        showtimeFeignService.addShowtimeReservation(reservationId, showtimeId);
     }
 
 }
