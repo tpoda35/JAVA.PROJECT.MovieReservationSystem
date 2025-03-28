7fb1473536ba3cc65f1b3ccca77a6e1506659bf1
tpoda <tpoda35@gmail.com>
2025-03-28 12:39:21 +0100

    Changed the response dto.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/service/IReservationService.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/service/IReservationService.java
@@ -2,12 +2,13 @@ package com.moviereservationapi.reservation.service;
 
 import com.moviereservationapi.reservation.dto.ReservationDto;
 import com.moviereservationapi.reservation.dto.ReservationManageDto;
+import com.moviereservationapi.reservation.dto.ReservationResponseDto;
 import org.springframework.data.domain.Page;
 
 import java.util.concurrent.CompletableFuture;
 
 public interface IReservationService {
-    ReservationDto addReservation(ReservationManageDto reservationManageDto);
+    ReservationResponseDto addReservation(ReservationManageDto reservationManageDto);
     CompletableFuture<ReservationDto> getReservation(Long reservationId);
     void deleteReservation(Long reservationId);
     CompletableFuture<Page<ReservationDto>> getUserReservations(int pageNum, int pageSize, Long userId);
