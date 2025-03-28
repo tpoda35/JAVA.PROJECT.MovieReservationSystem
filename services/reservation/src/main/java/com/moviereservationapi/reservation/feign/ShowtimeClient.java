adc52714caf750aaa61a9d0296172ad23dc22cfd
tpoda <tpoda35@gmail.com>
2025-03-28 12:44:04 +0100

    Added addShowtimeReservation endpoint and changed the other endpoint name.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/feign/ShowtimeClient.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/feign/ShowtimeClient.java
@@ -1,13 +1,21 @@
 package com.moviereservationapi.reservation.feign;
 
+import com.moviereservationapi.reservation.dto.ShowtimeDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "showtime-service", url = "http://localhost:8092")
 public interface ShowtimeClient {
 
-    @GetMapping("/api/showtimes/feign/showtimeExists/{showtimeId}")
-    Boolean showtimeExists(@PathVariable("showtimeId") Long showtimeId);
+    @GetMapping("/api/showtimes/feign/getShowtime/{showtimeId}")
+    ShowtimeDto getShowtime(@PathVariable("showtimeId") Long showtimeId);
+
+    @PostMapping("/api/showtimes/feign/addShowtimeReservation/{showtimeId}/{reservationId}")
+    void addShowtimeReservation(
+            @PathVariable("showtimeId") Long showtimeId,
+            @PathVariable("reservationId") Long reservationId
+    );
 
 }
