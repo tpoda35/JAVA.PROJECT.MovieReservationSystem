1686a41f3e91d6af537e960bb19e533d56c2be1e
tpoda <tpoda35@gmail.com>
2025-03-28 12:39:10 +0100

    Added addShowtimeToMovie

--- services/movie/src/main/java/com/moviereservationapi/movie/service/IMovieFeignService.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/service/IMovieFeignService.java
@@ -2,4 +2,5 @@ package com.moviereservationapi.movie.service;
 
 public interface IMovieFeignService {
     Boolean movieExists(Long movieId);
+    void addShowtimeToMovie(Long movieId, Long showtimeId);
 }
