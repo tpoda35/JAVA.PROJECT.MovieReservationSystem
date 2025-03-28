37bf9635e37fdf18be992cc3dd1d46484d870122
tpoda <tpoda35@gmail.com>
2025-03-28 12:41:02 +0100

    Implemented addShowtimeToMovie.

--- services/movie/src/main/java/com/moviereservationapi/movie/service/impl/MovieFeignService.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/service/impl/MovieFeignService.java
@@ -1,7 +1,10 @@
 package com.moviereservationapi.movie.service.impl;
 
+import com.moviereservationapi.movie.exception.MovieNotFoundException;
+import com.moviereservationapi.movie.model.Movie;
 import com.moviereservationapi.movie.repository.MovieRepository;
 import com.moviereservationapi.movie.service.IMovieFeignService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,5 +21,15 @@ public class MovieFeignService implements IMovieFeignService {
         boolean exists = movieRepository.existsById(movieId);
         log.info("(Feign call) Movie with the id of {} exists: {}.", movieId, exists);
         return exists;
+    }
+
+    @Override
+    @Transactional
+    public void addShowtimeToMovie(Long movieId, Long showtimeId) {
+        Movie movie = movieRepository.findById(movieId)
+                .orElseThrow(() -> new MovieNotFoundException("Movie not found."));
+
+        movie.addShowtimeId(showtimeId);
+        movieRepository.save(movie);
     }
 }
