305df063ec0668dd222feb0c7e6caf4c71c7f133
tpoda <tpoda35@gmail.com>
2025-03-28 12:43:09 +0100

    Added seatDto for feign calls.

---
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/dto/SeatDto.java
@@ -0,0 +1,19 @@
+package com.moviereservationapi.reservation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SeatDto {
+
+    private Long id;
+    private String seatRow;
+    private Integer seatNumber;
+    private String roomName;
+
+}
