3aa78e85981e411a2954112c0ae57d03eb2a62dd
tpoda <tpoda35@gmail.com>
2025-03-29 19:18:37 +0100

    Added Dto for cinema.

---
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/dto/CinemaManageDto.java
@@ -0,0 +1,21 @@
+package com.moviereservationapi.cinema.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CinemaManageDto {
+
+    @NotBlank(message = "Cinema name cannot be empty.")
+    private String name;
+
+    @NotBlank(message = "Cinema location cannot be empty.")
+    private String location;
+
+}
