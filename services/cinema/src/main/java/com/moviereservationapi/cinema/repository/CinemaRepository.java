79a53818d6713aafe58c2fbed0944569ad38397c
tpoda <tpoda35@gmail.com>
2025-03-30 15:06:55 +0200

    Added custom query to fetch the rooms by cinemaId.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/repository/CinemaRepository.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/repository/CinemaRepository.java
@@ -1,9 +1,18 @@
 package com.moviereservationapi.cinema.repository;
 
 import com.moviereservationapi.cinema.model.Cinema;
+import com.moviereservationapi.cinema.model.Room;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
+
+    @Query("SELECT r FROM Room r WHERE r.cinema.id = :cinemaId")
+    Page<Room> findAllRoomsByCinemaId(@Param("cinemaId") Long cinemaId, Pageable pageable);
+
 }
