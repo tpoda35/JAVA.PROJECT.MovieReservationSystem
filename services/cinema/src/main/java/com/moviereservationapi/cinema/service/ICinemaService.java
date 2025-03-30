4c45dba44bd7d557ca3be0ce4f109f95b2bf22ef
tpoda <tpoda35@gmail.com>
2025-03-30 15:07:55 +0200

    Renamed Dtos.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/service/ICinemaService.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/service/ICinemaService.java
@@ -1,16 +1,16 @@
 package com.moviereservationapi.cinema.service;
 
 import com.moviereservationapi.cinema.dto.CinemaManageDto;
-import com.moviereservationapi.cinema.dto.CinemaDetailsDto;
-import com.moviereservationapi.cinema.dto.CinemaDto;
+import com.moviereservationapi.cinema.dto.CinemaDetailsDtoV1;
+import com.moviereservationapi.cinema.dto.CinemaDetailsDtoV2;
 import org.springframework.data.domain.Page;
 
 import java.util.concurrent.CompletableFuture;
 
 public interface ICinemaService {
-    CompletableFuture<Page<CinemaDetailsDto>> getCinemas(int pageNum, int pageSize);
-    CompletableFuture<CinemaDetailsDto> getCinema(Long cinemaId);
-    CinemaDetailsDto addCinema(CinemaManageDto cinemaManageDto);
-    CinemaDto editCinema(CinemaManageDto cinemaManageDto, Long cinemaId);
+    CompletableFuture<Page<CinemaDetailsDtoV1>> getCinemas(int pageNum, int pageSize);
+    CompletableFuture<CinemaDetailsDtoV1> getCinema(Long cinemaId);
+    CinemaDetailsDtoV1 addCinema(CinemaManageDto cinemaManageDto);
+    CinemaDetailsDtoV2 editCinema(CinemaManageDto cinemaManageDto, Long cinemaId);
     void deleteCinema(Long cinemaId);
 }
