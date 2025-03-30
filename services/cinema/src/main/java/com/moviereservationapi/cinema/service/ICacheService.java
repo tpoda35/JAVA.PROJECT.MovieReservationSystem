10a318f49b3942f84a74ccb82f9c478c741f9a5b
tpoda <tpoda35@gmail.com>
2025-03-30 15:07:43 +0200

    Added interface for cache service.

---
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/service/ICacheService.java
@@ -0,0 +1,17 @@
+package com.moviereservationapi.cinema.service;
+
+import com.moviereservationapi.cinema.dto.CinemaDetailsDtoV1;
+import com.moviereservationapi.cinema.dto.RoomDetailsDtoV1;
+import com.moviereservationapi.cinema.dto.SeatDetailsDtoV1;
+import org.springframework.cache.Cache;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface ICacheService {
+    <T> void saveInCache(Cache cache, String cacheKey, T data, String logPrefix);
+    <T> T getCachedData(Cache cache, String cacheKey, String logPrefix, Class<T> type);
+    Page<CinemaDetailsDtoV1> getCachedCinemaPage(Cache cache, String cacheKey, String logPrefix);
+    List<SeatDetailsDtoV1> getCachedSeatList(Cache cache, String cacheKey, String logPrefix);
+    Page<RoomDetailsDtoV1> getCachedRoomPage(Cache cache, String cacheKey, String logPrefix);
+}
