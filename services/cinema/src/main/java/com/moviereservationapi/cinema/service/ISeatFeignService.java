9920b6e364d76f4ecc4c3bfb94afb865ef85ec25
tpoda35 <tpoda35@gmail.com>
2025-04-02 17:21:11 +0200

    Moved dtos.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/service/ISeatFeignService.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/service/ISeatFeignService.java
@@ -1,6 +1,6 @@
 package com.moviereservationapi.cinema.service;
 
-import com.moviereservationapi.cinema.dto.SeatDetailsDtoV1;
+import com.moviereservationapi.cinema.dto.seat.SeatDetailsDtoV1;
 
 import java.util.List;
 
