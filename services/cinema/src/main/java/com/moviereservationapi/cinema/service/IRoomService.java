b0ecd4baf8836c5173304b26845d42f0fbb753c3
tpoda35 <tpoda35@gmail.com>
2025-04-02 17:20:54 +0200

    Moved and renamed dtos.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/service/IRoomService.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/service/IRoomService.java
@@ -1,16 +1,16 @@
 package com.moviereservationapi.cinema.service;
 
-import com.moviereservationapi.cinema.dto.RoomDetailsDtoV1;
-import com.moviereservationapi.cinema.dto.RoomDetailsDtoV2;
-import com.moviereservationapi.cinema.dto.RoomManageDto;
+import com.moviereservationapi.cinema.dto.room.RoomDetailsDtoV1;
+import com.moviereservationapi.cinema.dto.room.RoomManageDtoV1;
+import com.moviereservationapi.cinema.dto.room.RoomManageDtoV2;
 import org.springframework.data.domain.Page;
 
 import java.util.concurrent.CompletableFuture;
 
 public interface IRoomService {
     CompletableFuture<Page<RoomDetailsDtoV1>> getRoomsByCinema(Long cinemaId, int pageSize, int pageNum);
-    CompletableFuture<RoomDetailsDtoV2> getRoom(Long roomId);
-    RoomDetailsDtoV1 addRoom(RoomManageDto roomManageDto);
-    RoomDetailsDtoV2 editRoom(RoomManageDto roomManageDto, Long roomId);
+    CompletableFuture<RoomDetailsDtoV1> getRoom(Long roomId);
+    RoomDetailsDtoV1 addRoom(RoomManageDtoV1 roomManageDtoV1);
+    RoomDetailsDtoV1 editRoom(RoomManageDtoV2 roomManageDtoV2, Long roomId);
     void deleteRoom(Long roomId);
 }
