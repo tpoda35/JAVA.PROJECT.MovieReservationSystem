f72d755df18f64b53642fe913f2942a116dd2412
tpoda35 <tpoda35@gmail.com>
2025-04-02 17:21:22 +0200

    Moved and renamed dtos.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/service/ISeatService.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/service/ISeatService.java
@@ -1,15 +1,16 @@
 package com.moviereservationapi.cinema.service;
 
-import com.moviereservationapi.cinema.dto.SeatDetailsDtoV1;
-import com.moviereservationapi.cinema.dto.SeatManageDto;
+import com.moviereservationapi.cinema.dto.seat.SeatCreateDto;
+import com.moviereservationapi.cinema.dto.seat.SeatDetailsDtoV1;
+import com.moviereservationapi.cinema.dto.seat.SeatEditDto;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public interface ISeatService {
-    CompletableFuture<SeatDetailsDtoV1> getSeat(Long seatId);
+    CompletableFuture<SeatDetailsDtoV1> getSeatById(Long seatId);
     CompletableFuture<List<SeatDetailsDtoV1>> getAllSeatByRoom(Long roomId);
-    SeatDetailsDtoV1 addSeat(SeatManageDto seatManageDto);
-    SeatDetailsDtoV1 editSeat(Long seatId, SeatManageDto seatManageDto);
+    SeatDetailsDtoV1 addSeat(SeatCreateDto SeatCreateDto);
+    SeatDetailsDtoV1 editSeat(Long seatId, SeatEditDto seatEditDto);
     void deleteSeat(Long seatId);
 }
