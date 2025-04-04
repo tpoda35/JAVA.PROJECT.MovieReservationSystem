e79e1bdcf0f780599698eac9449756386ef64800
tpoda <tpoda35@gmail.com>
2025-04-04 15:00:23 +0200

    Added dto for feign call.

---
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/dto/feign/SeatDto.java
@@ -0,0 +1,15 @@
+package com.moviereservationapi.showtime.dto.feign;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SeatDto implements Serializable {
+}
