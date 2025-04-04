b12562ccbfa7f2e3076c0001c8018dd7dd285988
tpoda <tpoda35@gmail.com>
2025-04-04 15:01:32 +0200

    Renamed DTO.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/mapper/ShowtimeMapper.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/mapper/ShowtimeMapper.java
@@ -1,25 +1,25 @@
 package com.moviereservationapi.showtime.mapper;
 
-import com.moviereservationapi.showtime.dto.ShowtimeDto;
-import com.moviereservationapi.showtime.dto.ShowtimeManageDto;
+import com.moviereservationapi.showtime.dto.ShowtimeDetailsDtoV1;
+import com.moviereservationapi.showtime.dto.ShowtimeCreateDto;
 import com.moviereservationapi.showtime.model.Showtime;
 
 public class ShowtimeMapper {
 
-    public static ShowtimeDto fromShowtimeToDto(Showtime showtime) {
-        return ShowtimeDto.builder()
+    public static ShowtimeDetailsDtoV1 fromShowtimeToDto(Showtime showtime) {
+        return ShowtimeDetailsDtoV1.builder()
                 .id(showtime.getId())
                 .startTime(showtime.getStartTime())
                 .endTime(showtime.getEndTime())
                 .build();
     }
 
-    public static Showtime fromManageDtoToShowtime(ShowtimeManageDto showtimeManageDto) {
+    public static Showtime fromManageDtoToShowtime(ShowtimeCreateDto showtimeCreateDto) {
         return Showtime.builder()
-                .startTime(showtimeManageDto.getStartTime())
-                .endTime(showtimeManageDto.getEndTime())
-                .movieId(showtimeManageDto.getMovieId())
-                .roomId(showtimeManageDto.getRoomId())
+                .startTime(showtimeCreateDto.getStartTime())
+                .endTime(showtimeCreateDto.getEndTime())
+                .movieId(showtimeCreateDto.getMovieId())
+                .roomId(showtimeCreateDto.getRoomId())
                 .build();
     }
 
