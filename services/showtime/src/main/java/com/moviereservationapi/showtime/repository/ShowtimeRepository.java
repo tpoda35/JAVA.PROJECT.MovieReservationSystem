a3fdb61f93c1e1bb68bdf8a3ed94d7e366efa8a2
tpoda <tpoda35@gmail.com>
2025-04-04 15:01:44 +0200

    Added query.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/repository/ShowtimeRepository.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/repository/ShowtimeRepository.java
@@ -1,9 +1,14 @@
 package com.moviereservationapi.showtime.repository;
 
 import com.moviereservationapi.showtime.model.Showtime;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShowtimeRepository extends JpaRepository<Showtime, Long> {
+
+    Page<Showtime> findByMovieId(Long movieId, Pageable pageable);
+
 }
