0ca23035d0b14ae74a46e9682e11756f860af094
tpoda <tpoda35@gmail.com>
2025-04-04 14:59:25 +0200

    Added methods.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/service/IShowtimeService.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/service/IShowtimeService.java
@@ -1,13 +1,18 @@
 package com.moviereservationapi.showtime.service;
 
-import com.moviereservationapi.showtime.dto.ShowtimeDto;
-import com.moviereservationapi.showtime.dto.ShowtimeManageDto;
+import com.moviereservationapi.showtime.dto.ShowtimeDetailsDtoV1;
+import com.moviereservationapi.showtime.dto.ShowtimeCreateDto;
+import com.moviereservationapi.showtime.dto.feign.SeatDto;
 import org.springframework.data.domain.Page;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public interface IShowtimeService {
-    CompletableFuture<Page<ShowtimeDto>> getShowtimes(int pageNum, int pageSize);
-    CompletableFuture<ShowtimeDto> getShowtime(Long showtimeId);
-    ShowtimeDto addShowtime(ShowtimeManageDto showtimeManageDto);
+    CompletableFuture<Page<ShowtimeDetailsDtoV1>> getShowtimes(int pageNum, int pageSize);
+    CompletableFuture<ShowtimeDetailsDtoV1> getShowtime(Long showtimeId);
+    ShowtimeDetailsDtoV1 addShowtime(ShowtimeCreateDto showtimeCreateDto);
+    CompletableFuture<Page<ShowtimeDetailsDtoV1>> getShowtimesByMovie(Long movieId, int pageNum, int pageSize);
+    CompletableFuture<List<SeatDto>> getSeatsByShowtime(Long showtimeId);
+    void deleteShowtime(Long showtimeId);
 }
