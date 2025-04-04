67a303e21f685cf1af58fad908fd48e0460883b9
tpoda <tpoda35@gmail.com>
2025-04-04 15:00:11 +0200

    Implemented addShowtimeToRoom.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/service/impl/RoomFeignService.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/service/impl/RoomFeignService.java
@@ -1,7 +1,10 @@
 package com.moviereservationapi.cinema.service.impl;
 
+import com.moviereservationapi.cinema.exception.RoomNotFoundException;
+import com.moviereservationapi.cinema.model.Room;
 import com.moviereservationapi.cinema.repository.RoomRepository;
 import com.moviereservationapi.cinema.service.IRoomFeignService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,10 +17,18 @@ public class RoomFeignService implements IRoomFeignService {
     private final RoomRepository roomRepository;
 
     @Override
-    public Boolean roomExists(Long roomId) {
-        boolean exists = roomRepository.existsById(roomId);
-        log.info("(Feign call) Room with the id of {} exists: {}.", roomId, exists);
-        return exists;
+    @Transactional
+    public void addShowtimeToRoom(Long showtimeId, Long roomId) {
+        Room room = findRoomById(roomId);
+        room.getShowtimeIds().add(showtimeId);
+        roomRepository.save(room);
     }
 
+    private Room findRoomById(Long roomId) {
+        return roomRepository.findById(roomId)
+                .orElseThrow(() -> {
+                    log.info("(Feign call) Room with the id of {} not found.", roomId);
+                    return new RoomNotFoundException("Room not found.");
+                });
+    }
 }
