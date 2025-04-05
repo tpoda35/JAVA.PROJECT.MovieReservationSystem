160e83e4349c1fd664e552c7b6d6a4ddbb158be6
tpoda <tpoda35@gmail.com>
2025-04-05 13:05:23 +0200

    Moved dto.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/exception/GlobalExceptionHandler.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.moviereservationapi.showtime.exception;
 
-import com.moviereservationapi.showtime.dto.CustomExceptionDto;
+import com.moviereservationapi.showtime.dto.exception.CustomExceptionDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
