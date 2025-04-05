7b0db48c947a2506a2c7e43c85f6029051cf237c
tpoda <tpoda35@gmail.com>
2025-04-05 13:07:12 +0200

    Added custom query for finding ReservedSeatIds by showtimeId.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/repository/ReservationSeatRepository.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/repository/ReservationSeatRepository.java
@@ -2,8 +2,16 @@ package com.moviereservationapi.reservation.repository;
 
 import com.moviereservationapi.reservation.model.ReservationSeat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ReservationSeatRepository extends JpaRepository<ReservationSeat, Long> {
+
+    @Query("SELECT rs.seatId FROM ReservationSeat rs WHERE rs.reservation.showtimeId = :showtimeId")
+    List<Long> findReservedSeatIdsByShowtimeId(@Param("showtimeId") Long showtimeId);
+
 }
