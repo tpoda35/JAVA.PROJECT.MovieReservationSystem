d02a77f1f03378e2d48278627be31af62bd13908
tpoda <tpoda35@gmail.com>
2025-04-05 13:04:33 +0200

    Added new feign endpoint.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/feign/CinemaClient.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/feign/CinemaClient.java
@@ -1,8 +1,12 @@
 package com.moviereservationapi.showtime.feign;
 
+import com.moviereservationapi.showtime.dto.feign.SeatDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @FeignClient(name = "cinema-service", url = "http://localhost:8091")
 public interface CinemaClient {
@@ -10,6 +14,11 @@ public interface CinemaClient {
     @PostMapping("/api/rooms/feign/addShowtimeToRoom/{showtimeId}/{roomId}")
     void addShowtimeToRoom(
             @PathVariable("showtimeId") Long showtimeId,
+            @PathVariable("roomId") Long roomId
+    );
+
+    @GetMapping("/api/seats/feign/getSeatsByRoomId/{roomId}")
+    List<SeatDto> getSeatsByRoomId(
             @PathVariable("roomId") Long roomId
     );
 
