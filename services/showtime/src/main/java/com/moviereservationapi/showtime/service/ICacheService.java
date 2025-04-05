a5d1a417db5d52911c9cd1bf768c97b02a318c6a
tpoda <tpoda35@gmail.com>
2025-04-05 13:05:26 +0200

    Moved dto.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/service/ICacheService.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/service/ICacheService.java
@@ -1,6 +1,6 @@
 package com.moviereservationapi.showtime.service;
 
-import com.moviereservationapi.showtime.dto.ShowtimeDetailsDtoV1;
+import com.moviereservationapi.showtime.dto.showtime.ShowtimeDetailsDtoV1;
 import org.springframework.cache.Cache;
 import org.springframework.data.domain.Page;
 
