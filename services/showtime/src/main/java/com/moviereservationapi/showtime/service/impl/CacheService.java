c1881e2adb2c419a116f92be247c5f6d6974036e
tpoda <tpoda35@gmail.com>
2025-04-05 13:04:23 +0200

    Renamed Dto.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/service/impl/CacheService.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/service/impl/CacheService.java
@@ -1,6 +1,6 @@
 package com.moviereservationapi.showtime.service.impl;
 
-import com.moviereservationapi.showtime.dto.ShowtimeDetailsDtoV1;
+import com.moviereservationapi.showtime.dto.showtime.ShowtimeDetailsDtoV1;
 import com.moviereservationapi.showtime.service.ICacheService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.Cache;
