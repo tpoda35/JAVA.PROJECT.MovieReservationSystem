25209482f95198db025e012eddb73ce8c3cdf939
tpoda <tpoda35@gmail.com>
2025-04-05 13:05:49 +0200

    Added new method.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/service/ISeatFeignService.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/service/ISeatFeignService.java
@@ -1,9 +1,11 @@
 package com.moviereservationapi.cinema.service;
 
 import com.moviereservationapi.cinema.dto.seat.SeatDetailsDtoV1;
+import com.moviereservationapi.cinema.dto.seat.SeatDto;
 
 import java.util.List;
 
 public interface ISeatFeignService {
     List<SeatDetailsDtoV1> getSeats(List<Long> seatIds);
+    List<SeatDto> getSeatsByRoomId(Long roomId);
 }
