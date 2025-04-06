0d699e1cc95198254e726f72ad6171400c16aea1
tpoda <tpoda35@gmail.com>
2025-04-06 16:22:36 +0200

    Import changed.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/exception/GlobalExceptionHandler.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.moviereservationapi.reservation.exception;
 
-import com.moviereservationapi.reservation.dto.CustomExceptionDto;
+import com.moviereservationapi.reservation.dto.exception.CustomExceptionDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
