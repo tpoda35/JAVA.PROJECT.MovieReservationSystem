da9d7cd2f033d8701dcca745cf5a15cc26faf98b
tpoda <tpoda35@gmail.com>
2025-04-06 16:25:06 +0200

    Added new query.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/repository/ReservationSeatRepository.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/repository/ReservationSeatRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
@@ -14,4 +15,5 @@ public interface ReservationSeatRepository extends JpaRepository<ReservationSeat
     @Query("SELECT rs.seatId FROM ReservationSeat rs WHERE rs.reservation.showtimeId = :showtimeId")
     List<Long> findReservedSeatIdsByShowtimeId(@Param("showtimeId") Long showtimeId);
 
+    Boolean existsBySeatIdInAndReservation_ShowtimeId(Collection<Long> seatIds, Long reservationShowtimeId);
 }
