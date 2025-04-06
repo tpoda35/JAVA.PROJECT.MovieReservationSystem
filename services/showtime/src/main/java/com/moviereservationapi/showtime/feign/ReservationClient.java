be65f9b611d1802885ce7365b04b16ef448f6927
tpoda <tpoda35@gmail.com>
2025-04-06 14:00:00 +0200

    Added feign endpoint.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/feign/ReservationClient.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/feign/ReservationClient.java
@@ -1,6 +1,7 @@
 package com.moviereservationapi.showtime.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -11,6 +12,11 @@ public interface ReservationClient {
 
     @GetMapping("/api/reservationseats/feign/findReservedSeatIdsByShowtimeId/{showtimeId}")
     List<Long> findReservedSeatIdsByShowtimeId(
+            @PathVariable("showtimeId") Long showtimeId
+    );
+
+    @DeleteMapping("/api/reservations/feign/deleteReservationWithShowtimeId/{showtimeId}")
+    void deleteReservationWithShowtimeId(
             @PathVariable("showtimeId") Long showtimeId
     );
 
