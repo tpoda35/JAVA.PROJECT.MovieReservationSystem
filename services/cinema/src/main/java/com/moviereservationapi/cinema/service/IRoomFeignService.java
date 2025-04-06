230317bc2a24c3712621119602ffc3e0458f50cd
tpoda <tpoda35@gmail.com>
2025-04-06 13:59:14 +0200

    Added deleteShowtimeFromRoom.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/service/IRoomFeignService.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/service/IRoomFeignService.java
@@ -2,4 +2,5 @@ package com.moviereservationapi.cinema.service;
 
 public interface IRoomFeignService {
     void addShowtimeToRoom(Long showtimeId, Long roomId);
+    void deleteShowtimeFromRoom(Long showtimeId, Long roomId);
 }
