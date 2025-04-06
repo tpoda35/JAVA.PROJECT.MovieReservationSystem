005972121d4ae2fd8cdc896d8926c056ba88011d
tpoda <tpoda35@gmail.com>
2025-04-06 16:22:59 +0200

    Imports changed, renamed some dtos.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/service/IReservationService.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/service/IReservationService.java
@@ -1,15 +1,15 @@
 package com.moviereservationapi.reservation.service;
 
-import com.moviereservationapi.reservation.dto.ReservationDto;
-import com.moviereservationapi.reservation.dto.ReservationManageDto;
-import com.moviereservationapi.reservation.dto.ReservationResponseDto;
+import com.moviereservationapi.reservation.dto.reservation.ReservationDetailsDtoV1;
+import com.moviereservationapi.reservation.dto.reservation.ReservationCreateDto;
+import com.moviereservationapi.reservation.dto.reservation.ReservationResponseDto;
 import org.springframework.data.domain.Page;
 
 import java.util.concurrent.CompletableFuture;
 
 public interface IReservationService {
-    ReservationResponseDto addReservation(ReservationManageDto reservationManageDto);
-    CompletableFuture<ReservationDto> getReservation(Long reservationId);
+    ReservationResponseDto addReservation(ReservationCreateDto reservationCreateDto);
+    CompletableFuture<ReservationDetailsDtoV1> getReservation(Long reservationId);
     void deleteReservation(Long reservationId);
-    CompletableFuture<Page<ReservationDto>> getUserReservations(int pageNum, int pageSize, Long userId);
+    CompletableFuture<Page<ReservationDetailsDtoV1>> getUserReservations(int pageNum, int pageSize, Long userId);
 }
