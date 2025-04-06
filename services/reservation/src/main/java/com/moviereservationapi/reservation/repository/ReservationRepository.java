0d0ab45f8ed005e498952533f5c9e85625f2db98
tpoda <tpoda35@gmail.com>
2025-04-06 14:01:14 +0200

    Added new query.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/repository/ReservationRepository.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/repository/ReservationRepository.java
@@ -4,6 +4,11 @@ import com.moviereservationapi.reservation.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    void removeAllByShowtimeId(Long showtimeId);
+
 }
