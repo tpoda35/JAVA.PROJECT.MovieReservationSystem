1e4fde2e555b32d70350f2e306ed0d7a3568f345
tpoda35 <tpoda35@gmail.com>
2025-04-08 20:03:25 +0200

    Added new dto for feign calls.

---
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/dto/showtime/ShowtimeDetailsDtoV2.java
@@ -0,0 +1,29 @@
+package com.moviereservationapi.showtime.dto.showtime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ShowtimeDetailsDtoV2 {
+
+    private Long id;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime startTime;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime endTime;
+
+    private Long roomId;
+}
