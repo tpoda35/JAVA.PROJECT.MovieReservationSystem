7824b0cc48c06e6be71dc16e7082fef342f28606
tpoda35 <tpoda35@gmail.com>
2025-04-08 20:01:32 +0200

    Changed return dto.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/service/ISeatFeignService.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/service/ISeatFeignService.java
@@ -1,11 +1,12 @@
 package com.moviereservationapi.cinema.service;
 
 import com.moviereservationapi.cinema.dto.seat.SeatDetailsDtoV1;
+import com.moviereservationapi.cinema.dto.seat.SeatDetailsDtoV2;
 import com.moviereservationapi.cinema.dto.seat.SeatDto;
 
 import java.util.List;
 
 public interface ISeatFeignService {
-    List<SeatDetailsDtoV1> getSeats(List<Long> seatIds);
+    List<SeatDetailsDtoV2> getSeats(List<Long> seatIds);
     List<SeatDto> getSeatsByRoomId(Long roomId);
 }
