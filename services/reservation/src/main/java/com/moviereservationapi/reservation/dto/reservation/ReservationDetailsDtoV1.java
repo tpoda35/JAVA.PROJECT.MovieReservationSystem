c0570c2d7bb01d9824322e358d23df1229f8df37
tpoda35 <tpoda35@gmail.com>
2025-04-08 18:32:36 +0200

    Added Serializable for caching.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/dto/reservation/ReservationDetailsDtoV1.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/dto/reservation/ReservationDetailsDtoV1.java
@@ -1,7 +1,7 @@
 package com.moviereservationapi.reservation.dto.reservation;
 
 import com.moviereservationapi.reservation.Enum.PaymentStatus;
-import com.moviereservationapi.reservation.model.ReservationSeat;
+import com.moviereservationapi.reservation.dto.feign.SeatDto;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
@@ -9,6 +9,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,13 +17,13 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ReservationDetailsDtoV1 {
+public class ReservationDetailsDtoV1 implements Serializable {
 
     private LocalDateTime reservationTime;
 
     @Enumerated(EnumType.STRING)
     private PaymentStatus paymentStatus;
 
-    private List<ReservationSeat> reservationSeats;
+    private List<SeatDto> seatDtos;
 
 }
