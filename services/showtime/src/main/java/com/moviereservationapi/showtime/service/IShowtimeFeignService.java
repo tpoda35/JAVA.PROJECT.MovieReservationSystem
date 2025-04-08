89b777e09a46b0794d4b98f6ec783aecffc19cd0
tpoda35 <tpoda35@gmail.com>
2025-04-08 20:01:36 +0200

    Changed return dto.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/service/IShowtimeFeignService.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/service/IShowtimeFeignService.java
@@ -1,8 +1,8 @@
 package com.moviereservationapi.showtime.service;
 
-import com.moviereservationapi.showtime.dto.showtime.ShowtimeDetailsDtoV1;
+import com.moviereservationapi.showtime.dto.showtime.ShowtimeDetailsDtoV2;
 
 public interface IShowtimeFeignService {
-    ShowtimeDetailsDtoV1 getShowtime(Long showtimeId);
+    ShowtimeDetailsDtoV2 getShowtime(Long showtimeId);
     void addShowtimeReservation(Long reservationId, Long showtimeId);
 }
