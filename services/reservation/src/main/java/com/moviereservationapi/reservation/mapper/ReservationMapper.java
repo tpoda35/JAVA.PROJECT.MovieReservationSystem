03e02e615cbcec950db52290bfc4e3abc62828b0
tpoda35 <tpoda35@gmail.com>
2025-04-08 18:32:59 +0200

    Added new mapping.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/mapper/ReservationMapper.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/mapper/ReservationMapper.java
@@ -1,5 +1,7 @@
 package com.moviereservationapi.reservation.mapper;
 
+import com.moviereservationapi.reservation.dto.reservation.ReservationDetailsDtoV1;
+import com.moviereservationapi.reservation.dto.reservation.ReservationDetailsDtoV2;
 import com.moviereservationapi.reservation.dto.reservation.ReservationResponseDto;
 import com.moviereservationapi.reservation.dto.feign.SeatDto;
 import com.moviereservationapi.reservation.dto.feign.ShowtimeDto;
@@ -20,6 +22,27 @@ public class ReservationMapper {
                 .paymentStatus(reservation.getPaymentStatus())
                 .seatDtos(seatDtos)
                 .showtimeDto(showtimeDto)
+                .build();
+    }
+
+    public static ReservationDetailsDtoV1 fromReservationToDetailsDtoV1(
+            Reservation reservation,
+            List<SeatDto> seatDtos
+    ) {
+        return ReservationDetailsDtoV1.builder()
+                .reservationTime(reservation.getReservationTime())
+                .paymentStatus(reservation.getPaymentStatus())
+                .seatDtos(seatDtos)
+                .build();
+    }
+
+    public static ReservationDetailsDtoV2 fromReservationToDetailsDtoV2(
+            Reservation reservation
+    ) {
+        return ReservationDetailsDtoV2.builder()
+                .id(reservation.getId())
+                .reservationTime(reservation.getReservationTime())
+                .paymentStatus(reservation.getPaymentStatus())
                 .build();
     }
 
