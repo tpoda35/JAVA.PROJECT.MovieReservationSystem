960f9dc6678612b16e5afe15c1ffafbd2816f923
tpoda35 <tpoda35@gmail.com>
2025-04-08 20:02:45 +0200

    Changed dto and added transactional.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/service/impl/SeatFeignService.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/service/impl/SeatFeignService.java
@@ -1,12 +1,14 @@
 package com.moviereservationapi.cinema.service.impl;
 
 import com.moviereservationapi.cinema.dto.seat.SeatDetailsDtoV1;
+import com.moviereservationapi.cinema.dto.seat.SeatDetailsDtoV2;
 import com.moviereservationapi.cinema.dto.seat.SeatDto;
 import com.moviereservationapi.cinema.exception.SeatNotFoundException;
 import com.moviereservationapi.cinema.mapper.SeatMapper;
 import com.moviereservationapi.cinema.model.Seat;
 import com.moviereservationapi.cinema.repository.SeatRepository;
 import com.moviereservationapi.cinema.service.ISeatFeignService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,7 +23,8 @@ public class SeatFeignService implements ISeatFeignService {
     private final SeatRepository seatRepository;
 
     @Override
-    public List<SeatDetailsDtoV1> getSeats(List<Long> seatIds) {
+    @Transactional
+    public List<SeatDetailsDtoV2> getSeats(List<Long> seatIds) {
         List<Seat> seats = seatRepository.findAllById(seatIds);
 
         int seatsSize = seats.size();
@@ -34,7 +37,7 @@ public class SeatFeignService implements ISeatFeignService {
         log.info("(Feign call) Found all seats.");
 
         return seats.stream()
-                .map(SeatMapper::fromSeatToDetailsDtoV1)
+                .map(SeatMapper::fromSeatToDetailsDtoV2)
                 .toList();
     }
 
