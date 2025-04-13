a30b0683f230c5d43c4099b935f6d435aaa08f3a
tpoda <tpoda35@gmail.com>
2025-04-13 19:52:16 +0200

    Added exception when something happens with the payment.

---
+++ services/payment/src/main/java/com/moviereservationapi/payment/exception/PaymentException.java
@@ -0,0 +1,11 @@
+package com.moviereservationapi.payment.exception;
+
+public class PaymentException extends RuntimeException {
+    public PaymentException(String message) {
+        super(message);
+    }
+
+    public PaymentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
