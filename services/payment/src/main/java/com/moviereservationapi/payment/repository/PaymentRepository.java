f3fb087f030449a15e40ce0241dacdddff94837c
tpoda <tpoda35@gmail.com>
2025-04-13 19:52:33 +0200

    Added repository for payment.

---
+++ services/payment/src/main/java/com/moviereservationapi/payment/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.moviereservationapi.payment.repository;
+
+import com.moviereservationapi.payment.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
