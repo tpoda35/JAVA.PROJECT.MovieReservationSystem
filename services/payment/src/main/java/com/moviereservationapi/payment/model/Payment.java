9699aafda73160ea61fe34a79869fa9e1413ceae
tpoda <tpoda35@gmail.com>
2025-04-13 19:51:30 +0200

    Added payment model.

---
+++ services/payment/src/main/java/com/moviereservationapi/payment/model/Payment.java
@@ -0,0 +1,43 @@
+package com.moviereservationapi.payment.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table (
+        indexes = {
+                @Index(name = "idx_showtime_id", columnList = "showtimeId")
+        }
+)
+public class Payment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long showtimeId;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "payment_seats",
+            joinColumns = @JoinColumn(name = "payment_id")
+    )
+    @Column(name = "seat_id")
+    @ToString.Exclude
+    private List<Long> seatIds = new ArrayList<>();
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
