eea5448caeacc26cd7118eea168a666958405121
tpoda <tpoda35@gmail.com>
2025-04-13 19:53:21 +0200

    Added dto for seats.

---
+++ services/payment/src/main/java/com/moviereservationapi/payment/dto/SeatDto.java
@@ -0,0 +1,20 @@
+package com.moviereservationapi.payment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SeatDto {
+
+    private Long id;
+    private String seatRow;
+    private Integer seatNumber;
+    private String roomName;
+    private Long roomId;
+
+}
