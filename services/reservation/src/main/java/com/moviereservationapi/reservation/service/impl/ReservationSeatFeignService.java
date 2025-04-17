409a815938aa873710cf954786e0ccb980a94c29
tpoda <tpoda35@gmail.com>
2025-04-17 17:06:09 +0200

    Implemented findSeatIdsByReservationId.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/service/impl/ReservationSeatFeignService.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/service/impl/ReservationSeatFeignService.java
@@ -1,7 +1,12 @@
 package com.moviereservationapi.reservation.service.impl;
 
+import com.moviereservationapi.reservation.exception.ReservationNotFoundException;
+import com.moviereservationapi.reservation.model.Reservation;
+import com.moviereservationapi.reservation.model.ReservationSeat;
+import com.moviereservationapi.reservation.repository.ReservationRepository;
 import com.moviereservationapi.reservation.repository.ReservationSeatRepository;
 import com.moviereservationapi.reservation.service.IReservationSeatFeignService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,9 +19,27 @@ import java.util.List;
 public class ReservationSeatFeignService implements IReservationSeatFeignService {
 
     private final ReservationSeatRepository reservationSeatRepository;
+    private final ReservationRepository reservationRepository;
 
     @Override
     public List<Long> findReservedSeatIdsByShowtimeId(Long showtimeId) {
         return reservationSeatRepository.findReservedSeatIdsByShowtimeId(showtimeId);
+    }
+
+    @Override
+    @Transactional
+    public List<Long> findSeatIdsByReservationId(Long reservationId) {
+        return findReservationById(reservationId).getReservationSeats()
+                .stream()
+                .map(ReservationSeat::getSeatId)
+                .toList();
+    }
+
+    private Reservation findReservationById(Long reservationId) {
+        return reservationRepository.findById(reservationId)
+                .orElseThrow(() -> {
+                    log.info("(Feign call) Reservation with the id of {} not found.", reservationId);
+                    return new ReservationNotFoundException("Reservation not found.");
+                });
     }
 }
