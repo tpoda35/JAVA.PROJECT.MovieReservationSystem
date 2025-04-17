066243f0bcea9257a3dda77e63f55e65431cf7af
tpoda <tpoda35@gmail.com>
2025-04-17 17:04:13 +0200

    Reworked and optimized the checkout system.

--- services/payment/src/main/java/com/moviereservationapi/payment/service/impl/PaymentService.java
+++ services/payment/src/main/java/com/moviereservationapi/payment/service/impl/PaymentService.java
@@ -1,25 +1,20 @@
 package com.moviereservationapi.payment.service.impl;
 
-import com.moviereservationapi.payment.dto.ReservationPaymentRequest;
-import com.moviereservationapi.payment.dto.SeatDto;
-import com.moviereservationapi.payment.dto.StripeResponse;
+import com.moviereservationapi.payment.dto.payment.StripeResponse;
 import com.moviereservationapi.payment.exception.PaymentException;
-import com.moviereservationapi.payment.exception.SeatNotFoundException;
-import com.moviereservationapi.payment.repository.PaymentRepository;
+import com.moviereservationapi.payment.feign.ReservationClient;
 import com.moviereservationapi.payment.service.IPaymentService;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
 import jakarta.annotation.PostConstruct;
-import jakarta.validation.Valid;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Slf4j
@@ -31,17 +26,17 @@ public class PaymentService implements IPaymentService {
     private final String successUrl;
     private final String cancelUrl;
 
-    private final PaymentRepository paymentRepository;
+    private final ReservationClient reservationClient;
 
     public PaymentService(
-            PaymentRepository paymentRepository,
+            ReservationClient reservationClient,
             @Value("${apiKey.stripeKey}") String stripeKey,
             @Value("${payment.default_currency}") String defaultCurrency,
             @Value("${payment.unit_amount}") long unitAmount,
             @Value("${payment.success_url}") String successUrl,
             @Value("${payment.cancel_url}") String cancelUrl
     ) {
-        this.paymentRepository = paymentRepository;
+        this.reservationClient = reservationClient;
         this.stripeKey = stripeKey;
         this.defaultCurrency = defaultCurrency;
         this.unitAmount = unitAmount;
@@ -54,28 +49,27 @@ public class PaymentService implements IPaymentService {
         Stripe.apiKey = stripeKey;
     }
 
-
-
     @Override
-    public StripeResponse checkout(@Valid ReservationPaymentRequest request) {
-        List<SeatDto> seats = request.getSeatDtos();
-        if (seats == null || seats.isEmpty()) {
-            throw new SeatNotFoundException("Seat(s) not found.");
+    @Transactional
+    public StripeResponse checkout(Long reservationId, String currency) {
+        String LOG_PREFIX = "checkout";
+
+        // Change this to a class and get the showtimeId also to save it in the webhookservice.
+        List<Long> seatIds = reservationClient.findSeatIdsByReservationId(reservationId);
+        long seatNum = seatIds.size();
+
+        if (currency == null) {
+            currency = defaultCurrency;
         }
-        long seatNum = seats.size();
-        String currency = Optional.ofNullable(request.getCurrency()).orElse(defaultCurrency);
-        LocalDateTime showtimeStartDate = request.getShowtimeDto().getStartTime();
 
         SessionCreateParams.LineItem lineItem = new SessionCreateParams.LineItem.Builder()
                 .setPriceData(
                         new SessionCreateParams.LineItem.PriceData.Builder()
                                 .setCurrency(currency)
-                                .setUnitAmount(unitAmount) // $10.00 per seat
+                                .setUnitAmount(unitAmount)
                                 .setProductData(
                                         new SessionCreateParams.LineItem.PriceData.ProductData.Builder()
                                                 .setName("Movie Seat Reservation")
-                                                .putMetadata("showtime", showtimeStartDate.toString())
-                                                .putMetadata("seats", String.valueOf(seatNum))
                                                 .build()
                                 )
                                 .build()
@@ -83,22 +77,28 @@ public class PaymentService implements IPaymentService {
                 .setQuantity(seatNum)
                 .build();
 
+
+        reservationClient.changeStatusToUnder_Payment(reservationId);
+
         SessionCreateParams params = SessionCreateParams.builder()
                 .setMode(SessionCreateParams.Mode.PAYMENT)
                 .setSuccessUrl(successUrl)
                 .setCancelUrl(cancelUrl)
                 .addLineItem(lineItem)
+                .putMetadata("reservationId", String.valueOf(reservationId))
+                .putMetadata("seatIds", String.valueOf(seatIds))
                 .build();
 
-        Session session = null;
+        Session session;
 
         try {
             session = Session.create(params);
         } catch (StripeException exception) {
-            // log
-            throw new PaymentException(exception.getMessage() , exception);
+            log.info("{} :: An exception threw while creating session: {}.", LOG_PREFIX, exception.getMessage());
+            throw new PaymentException(exception.getMessage(), exception);
         }
 
+        log.info("{} :: Creating payment session for the reservationId of {}.", LOG_PREFIX, reservationId);
         return StripeResponse.builder()
                 .status("SUCCESS")
                 .message("Payment session created.")
@@ -106,5 +106,4 @@ public class PaymentService implements IPaymentService {
                 .sessionUrl(session.getUrl())
                 .build();
     }
-    // If success -> database save, else only just a message that it is failed and delete the reservation (maybe).
 }
