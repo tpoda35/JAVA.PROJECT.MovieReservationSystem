99242fb540e7e89358e9b040dda8fdc5daa3553d
tpoda <tpoda35@gmail.com>
2025-04-17 17:05:22 +0200

    Deleted.

--- services/payment/src/main/java/com/moviereservationapi/payment/exception/SeatNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.moviereservationapi.payment.exception;
-
-public class SeatNotFoundException extends RuntimeException {
-    public SeatNotFoundException(String message) {
-        super(message);
-    }
-}
