b1999daac07fa879514a4ad4962cd1911b47fd13
tpoda <tpoda35@gmail.com>
2025-04-17 17:02:38 +0200

    Added 3 status change method.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/service/IReservationFeignService.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/service/IReservationFeignService.java
@@ -2,4 +2,7 @@ package com.moviereservationapi.reservation.service;
 
 public interface IReservationFeignService {
     void deleteReservationWithShowtimeId(Long showtimeId);
+    void changeStatusToPaid(Long reservationId);
+    void changeStatusToFailed(Long reservationId);
+    void changeStatusToUnder_Payment(Long reservationId);
 }
