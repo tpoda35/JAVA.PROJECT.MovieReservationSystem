657e638da8ca7cf1ec1a64b948140f5a4a0e17b2
tpoda <tpoda35@gmail.com>
2025-04-18 19:13:40 +0200

    Added exception handling for PaymentException.

--- services/payment/src/main/java/com/moviereservationapi/payment/exception/GlobalExceptionHandler.java
+++ services/payment/src/main/java/com/moviereservationapi/payment/exception/GlobalExceptionHandler.java
@@ -7,10 +7,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(PaymentException.class)
+    public ResponseEntity<CustomExceptionDto> handlePaymentException(PaymentException ex) {
+        return ResponseEntity.status(BAD_REQUEST).body(
+                new CustomExceptionDto(
+                        ex.getMessage(),
+                        LocalDateTime.now(),
+                        BAD_REQUEST.value()
+                )
+        );
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<CustomExceptionDto> handleUncaughtExceptions(Exception ex) {
