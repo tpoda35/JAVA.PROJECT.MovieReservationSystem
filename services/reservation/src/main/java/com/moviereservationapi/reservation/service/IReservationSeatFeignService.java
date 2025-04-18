0b2581c58a9fe437f702efb7064a68051277a84d
tpoda <tpoda35@gmail.com>
2025-04-18 19:14:21 +0200

    Removed a method.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/service/IReservationSeatFeignService.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/service/IReservationSeatFeignService.java
@@ -1,10 +1,7 @@
 package com.moviereservationapi.reservation.service;
 
-import com.moviereservationapi.reservation.dto.reservation.ReservationDetailsDtoV3;
-
 import java.util.List;
 
 public interface IReservationSeatFeignService {
     List<Long> findReservedSeatIdsByShowtimeId(Long showtimeId);
-    ReservationDetailsDtoV3 findSeatIdsAndShowtimeIdByReservationId(Long reservationId);
 }
