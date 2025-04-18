d5ba55cd772c9643ab75a4850eb5c2d798894db3
tpoda <tpoda35@gmail.com>
2025-04-18 19:19:58 +0200

    Added custom query for finding the overlapping showtimes.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/repository/ShowtimeRepository.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/repository/ShowtimeRepository.java
@@ -4,11 +4,24 @@ import com.moviereservationapi.showtime.model.Showtime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface ShowtimeRepository extends JpaRepository<Showtime, Long> {
 
     Page<Showtime> findByMovieId(Long movieId, Pageable pageable);
+
+    @Query("SELECT s FROM Showtime s WHERE s.roomId = :roomId AND " +
+            "(:startTime < s.endTime AND :endTime > s.startTime)")
+    List<Showtime> findOverlappingShowtimes(
+            @Param("roomId") Long roomId,
+            @Param("startTime") LocalDateTime startTime,
+            @Param("endTime") LocalDateTime endTime
+    );
 
 }
