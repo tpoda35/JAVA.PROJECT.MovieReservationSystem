f6ba8c979e6e1aebda3e9766acaca20715119081
tpoda <tpoda35@gmail.com>
2025-04-18 19:19:17 +0200

    Added versioning and ToString.Exclude.

--- services/showtime/src/main/java/com/moviereservationapi/showtime/model/Showtime.java
+++ services/showtime/src/main/java/com/moviereservationapi/showtime/model/Showtime.java
@@ -1,9 +1,7 @@
 package com.moviereservationapi.showtime.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -36,5 +34,9 @@ public class Showtime {
     @Column(name = "reservation_id")
     @ToString.Exclude
     private List<Long> reservationIds;
+
+    @Version
+    @ToString.Exclude
+    private Long version;
 
 }
