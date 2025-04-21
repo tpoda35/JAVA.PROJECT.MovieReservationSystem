426852106bd76983fd637b4b306127ed9d01ed79
tpoda <tpoda35@gmail.com>
2025-04-21 15:55:29 +0200

    Added objectMapper config for dates.

---
+++ services/payment/src/main/java/com/moviereservationapi/payment/config/ObjectMapperConfig.java
@@ -0,0 +1,18 @@
+package com.moviereservationapi.payment.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+
+}
