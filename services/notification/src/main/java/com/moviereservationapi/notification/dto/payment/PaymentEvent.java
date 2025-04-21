df532cd5da2b80718274ff67e6cacc61a005b2f9
tpoda <tpoda35@gmail.com>
2025-04-21 15:56:15 +0200

    Added json object for kafka.

---
+++ services/notification/src/main/java/com/moviereservationapi/notification/dto/payment/PaymentEvent.java
@@ -0,0 +1,24 @@
+package com.moviereservationapi.notification.dto.payment;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentEvent {
+
+    private Long id;
+    private Long showtimeId;
+    private List<Long> seatIds = new ArrayList<>();
+    private Long userId;
+    private LocalDateTime createdAt;
+
+}
