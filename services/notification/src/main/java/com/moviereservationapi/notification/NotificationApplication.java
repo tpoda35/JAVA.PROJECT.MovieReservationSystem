1e8039fba6de14a00219106128392966c76ed533
tpoda <tpoda35@gmail.com>
2025-04-21 15:52:20 +0200

    Added support for feign.

--- services/notification/src/main/java/com/moviereservationapi/notification/NotificationApplication.java
+++ services/notification/src/main/java/com/moviereservationapi/notification/NotificationApplication.java
@@ -2,8 +2,10 @@ package com.moviereservationapi.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class NotificationApplication {
 
 	public static void main(String[] args) {
