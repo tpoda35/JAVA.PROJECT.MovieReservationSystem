f0b8224296575dfd8185ee9a6244b6cf17e63f78
tpoda35 <tpoda35@gmail.com>
2025-04-22 13:19:57 +0200

    .

--- services/reservation/src/main/java/com/moviereservationapi/reservation/dto/reservation/ReservationCreateDto.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/dto/reservation/ReservationCreateDto.java
@@ -1,8 +1,6 @@
 package com.moviereservationapi.reservation.dto.reservation;
 
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
