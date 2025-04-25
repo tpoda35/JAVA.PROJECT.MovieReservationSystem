611d65a8e2db06ae4d4b7eb44113551d52d0865f
tpoda35 <tpoda35@gmail.com>
2025-04-25 14:24:58 +0200

    Removed unused imports.

--- services/movie/src/main/java/com/moviereservationapi/movie/controller/MovieController.java
+++ services/movie/src/main/java/com/moviereservationapi/movie/controller/MovieController.java
@@ -1,17 +1,12 @@
 package com.moviereservationapi.movie.controller;
 
 import com.moviereservationapi.movie.dto.MovieDto;
-import com.moviereservationapi.movie.dto.MovieManageDto;
 import com.moviereservationapi.movie.service.IMovieService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
