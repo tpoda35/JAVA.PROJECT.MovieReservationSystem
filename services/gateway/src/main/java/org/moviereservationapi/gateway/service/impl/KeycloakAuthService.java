7f0c90df8eed5de414c9b87f4fc7d76be9461db1
tpoda35 <tpoda35@gmail.com>
2025-04-28 19:53:46 +0200

    Implemented exchangeCodeForTokens.

--- services/gateway/src/main/java/org/moviereservationapi/gateway/service/impl/KeycloakAuthService.java
+++ services/gateway/src/main/java/org/moviereservationapi/gateway/service/impl/KeycloakAuthService.java
@@ -19,7 +19,7 @@ public class KeycloakAuthService implements IKeycloakAuthService {
 
     private final WebClient webClient;
 
-
+    // only for testing
     @Override
     public Mono<Map<String, Object>> getToken(String username, String password) {
         MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
@@ -40,4 +40,26 @@ public class KeycloakAuthService implements IKeycloakAuthService {
                 .bodyToMono(new ParameterizedTypeReference<>() {
                 });
     }
+
+    @Override
+    public Mono<Map<String, Object>> exchangeCodeForTokens(String code) {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("grant_type", "authorization_code");
+        formData.add("code", code);
+        formData.add("client_id", "movie-api");
+
+        return webClient.post()
+                .uri("http://localhost:8080/realms/movie-app/protocol/openid-connect/token")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .bodyValue(formData)
+                .retrieve()
+                .onStatus(HttpStatusCode::isError, clientResponse ->
+                        clientResponse.bodyToMono(String.class)
+                                .map(errorBody -> new RuntimeException("Keycloak authentication error: " + errorBody))
+                )
+                .bodyToMono(new ParameterizedTypeReference<>() {
+                });
+    }
+
+
 }
