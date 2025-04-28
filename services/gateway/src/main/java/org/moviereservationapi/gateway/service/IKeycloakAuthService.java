03cf938273f7f2cc85fc38bd2cf0571785c7f88a
tpoda35 <tpoda35@gmail.com>
2025-04-28 19:53:29 +0200

    Added code interface method.

--- services/gateway/src/main/java/org/moviereservationapi/gateway/service/IKeycloakAuthService.java
+++ services/gateway/src/main/java/org/moviereservationapi/gateway/service/IKeycloakAuthService.java
@@ -6,4 +6,5 @@ import java.util.Map;
 
 public interface IKeycloakAuthService {
     Mono<Map<String, Object>> getToken(String username, String password);
+    Mono<Map<String, Object>> exchangeCodeForTokens(String code);
 }
