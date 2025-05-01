ad3ac99811daeefcf579ed8e705cca1662ecdc94
tpoda35 <tpoda35@gmail.com>
2025-05-01 20:53:12 +0200

    Added user entity to store users distributed from the keycloak.

---
+++ services/user/src/main/java/org/moviereservationapi/user/model/AppUser.java
@@ -0,0 +1,25 @@
+package org.moviereservationapi.user.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class AppUser {
+
+    @Id
+    private String id;
+
+    private String email;
+
+
+}
