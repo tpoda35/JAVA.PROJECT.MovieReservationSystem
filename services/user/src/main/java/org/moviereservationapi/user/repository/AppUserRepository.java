298bb0a29071f85ba3938abe3076c11e482763e6
tpoda35 <tpoda35@gmail.com>
2025-05-01 20:53:20 +0200

    Added repo

---
+++ services/user/src/main/java/org/moviereservationapi/user/repository/AppUserRepository.java
@@ -0,0 +1,11 @@
+package org.moviereservationapi.user.repository;
+
+import org.moviereservationapi.user.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
+}
