479ee6343b4098568262ecdc12383f51012b9942
tpoda35 <tpoda35@gmail.com>
2025-05-01 20:54:14 +0200

    Added to include the JWT in the header because of the authentication.

---
+++ services/payment/src/main/java/com/moviereservationapi/payment/config/FeignClientInterceptor.java
@@ -0,0 +1,20 @@
+package com.moviereservationapi.payment.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        var context = SecurityContextHolder.getContext();
+        if (context != null && context.getAuthentication() instanceof JwtAuthenticationToken jwtAuth) {
+            String token = jwtAuth.getToken().getTokenValue();
+            requestTemplate.header("Authorization", "Bearer " + token);
+        }
+    }
+}
---
+++ services/user/src/main/java/org/moviereservationapi/user/config/FeignClientInterceptor.java
@@ -0,0 +1,20 @@
+package org.moviereservationapi.user.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        var context = SecurityContextHolder.getContext();
+        if (context != null && context.getAuthentication() instanceof JwtAuthenticationToken jwtAuth) {
+            String token = jwtAuth.getToken().getTokenValue();
+            requestTemplate.header("Authorization", "Bearer " + token);
+        }
+    }
+}
