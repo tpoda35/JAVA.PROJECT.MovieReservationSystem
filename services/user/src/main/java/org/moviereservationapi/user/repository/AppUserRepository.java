63897cac545d1026ec284d37aa9b9eedfc205d26
tpoda35 <tpoda35@gmail.com>
2025-05-02 12:55:10 +0200

    Changed Id type.

--- services/user/src/main/java/org/moviereservationapi/user/repository/AppUserRepository.java
+++ services/user/src/main/java/org/moviereservationapi/user/repository/AppUserRepository.java
@@ -4,8 +4,6 @@ import org.moviereservationapi.user.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
+public interface AppUserRepository extends JpaRepository<AppUser, String> {
 }
