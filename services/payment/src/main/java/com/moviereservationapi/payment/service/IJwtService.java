e00a422570a39e2c4fbc7d05347b913844776bc2
tpoda <tpoda35@gmail.com>
2025-05-09 00:37:53 +0200

    Added interface for service.

---
+++ services/payment/src/main/java/com/moviereservationapi/payment/service/IJwtService.java
@@ -0,0 +1,5 @@
+package com.moviereservationapi.payment.service;
+
+public interface IJwtService {
+    String getLoggedInUserEmailFromJwt();
+}
---
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/service/IJwtService.java
@@ -0,0 +1,5 @@
+package com.moviereservationapi.reservation.service;
+
+public interface IJwtService {
+    String getLoggedInUserIdFromJwt();
+}
