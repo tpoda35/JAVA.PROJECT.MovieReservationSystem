b5ff9c1d713b23ab10b07b756bf331c6cc8a0d86
tpoda <tpoda35@gmail.com>
2025-05-09 00:38:45 +0200

    Removed method

--- services/user/src/main/java/org/moviereservationapi/user/service/IUserFeignService.java
+++ services/user/src/main/java/org/moviereservationapi/user/service/IUserFeignService.java
@@ -1,8 +1,5 @@
 package org.moviereservationapi.user.service;
 
-import org.moviereservationapi.user.model.AppUser;
-
 public interface IUserFeignService {
-    AppUser getLoggedInUserOrCreateIfNotExists();
     void addReservationToUser(Long reservationId);
 }
