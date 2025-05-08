ada69b3a1c88d9d8eccb81519797514165e5e8b9
tpoda35 <tpoda35@gmail.com>
2025-05-08 11:17:21 +0200

    Added JPA interface method and changed userId type to String.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/repository/ReservationRepository.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/repository/ReservationRepository.java
@@ -1,17 +1,19 @@
 package com.moviereservationapi.reservation.repository;
 
+import com.moviereservationapi.reservation.Enum.PaymentStatus;
 import com.moviereservationapi.reservation.model.Reservation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     void removeAllByShowtimeId(Long showtimeId);
-    Page<Reservation> findByUserId(Long userId, Pageable pageable);
-
+    Page<Reservation> findByUserId(String userId, Pageable pageable);
+    List<Reservation> findByExpiresAtBeforeAndPaymentStatusNot(LocalDateTime expiresAt, PaymentStatus paymentStatus);
 }
