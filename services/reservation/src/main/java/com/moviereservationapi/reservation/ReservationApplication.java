dbb014a3f1b2f94f1b6ebeaa38ae30e58488a7aa
tpoda35 <tpoda35@gmail.com>
2025-05-08 11:14:51 +0200

    Enabled scheduling.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/ReservationApplication.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/ReservationApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
 @EnableCaching
 @EnableFeignClients
+@EnableScheduling
 public class ReservationApplication {
 
     public static void main(String[] args) {
