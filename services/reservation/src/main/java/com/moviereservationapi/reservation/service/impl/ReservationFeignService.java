0a2edee1389526582e6765cb71054dae67ea6ee0
tpoda35 <tpoda35@gmail.com>
2025-05-10 18:29:16 +0200

    Removed unnecessary implementations.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/service/impl/ReservationFeignService.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/service/impl/ReservationFeignService.java
@@ -1,11 +1,7 @@
 package com.moviereservationapi.reservation.service.impl;
 
-import com.moviereservationapi.reservation.dto.reservation.ReservationPayment;
-import com.moviereservationapi.reservation.exception.AlreadyPaidException;
 import com.moviereservationapi.reservation.exception.ReservationNotFoundException;
-import com.moviereservationapi.reservation.feign.UserClient;
 import com.moviereservationapi.reservation.model.Reservation;
-import com.moviereservationapi.reservation.model.ReservationSeat;
 import com.moviereservationapi.reservation.repository.ReservationRepository;
 import com.moviereservationapi.reservation.service.IReservationFeignService;
 import jakarta.transaction.Transactional;
@@ -13,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import static com.moviereservationapi.reservation.Enum.PaymentStatus.*;
+import static com.moviereservationapi.reservation.Enum.PaymentStatus.FAILED;
+import static com.moviereservationapi.reservation.Enum.PaymentStatus.PAID;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +18,6 @@ import static com.moviereservationapi.reservation.Enum.PaymentStatus.*;
 public class ReservationFeignService implements IReservationFeignService {
 
     private final ReservationRepository reservationRepository;
-    private final UserClient userClient;
 
     @Override
     @Transactional
@@ -47,28 +43,8 @@ public class ReservationFeignService implements IReservationFeignService {
 
     @Override
     @Transactional
-    public void changeStatusToUnder_Payment(Long reservationId) {
-        Reservation reservation = findReservationById(reservationId);
-        reservation.setExpiresAt(reservation.getExpiresAt().plusMinutes(15));
-        reservation.setPaymentStatus(UNDER_PAYMENT);
-        reservationRepository.save(reservation);
-    }
-
-    @Override
-    @Transactional
-    public ReservationPayment checkPaidAndGetPaymentDataByReservationId(Long reservationId) {
-        Reservation reservation = findReservationById(reservationId);
-
-        if (reservation.getPaymentStatus() == PAID) {
-            log.info("(Feign call) Reservation with the id of {} is already paid.", reservationId);
-            throw new AlreadyPaidException("Reservation already paid.");
-        }
-
-        return ReservationPayment.builder()
-                .seatIds(reservation.getReservationSeats().stream().map(ReservationSeat::getSeatId).toList())
-                .showtimeId(reservation.getShowtimeId())
-                .userId(reservation.getUserId())
-                .build();
+    public void deleteExpiredReservationById(Long reservationId) {
+        reservationRepository.deleteById(reservationId);
     }
 
     private Reservation findReservationById(Long reservationId) {
