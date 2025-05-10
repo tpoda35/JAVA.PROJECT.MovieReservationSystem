a3b15e077699dc08eadf30dc73bacdd9d841c295
tpoda35 <tpoda35@gmail.com>
2025-05-10 18:26:11 +0200

    Removed two method.

--- services/reservation/src/main/java/com/moviereservationapi/reservation/service/IReservationFeignService.java
+++ services/reservation/src/main/java/com/moviereservationapi/reservation/service/IReservationFeignService.java
@@ -1,11 +1,8 @@
 package com.moviereservationapi.reservation.service;
 
-import com.moviereservationapi.reservation.dto.reservation.ReservationPayment;
-
 public interface IReservationFeignService {
     void deleteReservationWithShowtimeId(Long showtimeId);
     void changeStatusToPaid(Long reservationId);
     void changeStatusToFailed(Long reservationId);
-    void changeStatusToUnder_Payment(Long reservationId);
-    ReservationPayment checkPaidAndGetPaymentDataByReservationId(Long reservationId);
+    void deleteExpiredReservationById(Long reservationId);
 }
