489834df4eba425edc6db3f67020e2c311a6b6b2
tpoda35 <tpoda35@gmail.com>
2025-05-10 18:25:52 +0200

    Changed required datatype.

--- services/payment/src/main/java/com/moviereservationapi/payment/service/IPaymentService.java
+++ services/payment/src/main/java/com/moviereservationapi/payment/service/IPaymentService.java
@@ -1,9 +1,10 @@
 package com.moviereservationapi.payment.service;
 
 import com.moviereservationapi.payment.dto.payment.StripeResponse;
+import com.moviereservationapi.payment.dto.reservation.ReservationPayment;
 
 public interface IPaymentService {
 
-    StripeResponse checkout(Long reservationId, String currency);
+    StripeResponse checkout(ReservationPayment reservationData, String currency);
 
 }
