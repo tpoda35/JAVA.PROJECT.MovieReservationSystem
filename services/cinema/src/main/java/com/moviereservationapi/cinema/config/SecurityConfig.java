54cc8d848944b101528cfe565df2bca56b6a00e1
tpoda35 <tpoda35@gmail.com>
2025-05-19 10:30:28 +0200

    Added prometheus and actuator dependencies, configured docker compose, removed authentication from metrics endpoint.

--- services/cinema/src/main/java/com/moviereservationapi/cinema/config/SecurityConfig.java
+++ services/cinema/src/main/java/com/moviereservationapi/cinema/config/SecurityConfig.java
@@ -22,11 +22,27 @@ import java.util.Map;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private static final String[] WHITE_LIST_URL = {
+            "/api/v1/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html",
+            "/actuator/prometheus"
+    };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(WHITE_LIST_URL).permitAll()
                         .anyRequest().authenticated()
                 )
                 .securityContext(context -> context
